9282c2a69791d7327a0a8603b146d21a58670be3
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-04 17:16:31 -0500

    remove literal::infix

--- src/literal.h
+++ src/literal.h
@@ -8,16 +8,14 @@ public:
 
 	inline Enode * 	get_enode()	{ return _e; }
 	void		mk_constraint( const char * );
-		
+
 private:
-  
+
 	vector< variable * >	v_list;
 	lbool polarity;	//whether it's negated
-	
+
 	Enode *			_e;	//original enode position
 	rp_constraint *		_c;
 	rp_table_symbol * 	_ts; //pointer to an outside symbol table
-	
-};
 
-void infix ( Enode *, string & s);
+};
