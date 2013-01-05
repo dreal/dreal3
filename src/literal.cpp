75fb8dc9180598b2b2193c306312fe8ff27ef1a5
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-05 15:40:48 -0500

    implement literal::infix

--- src/literal.cpp
+++ src/literal.cpp
@@ -11,74 +11,120 @@ literal::literal(Enode * e , rp_table_symbol * ts)
 literal::~literal()
 {}
 
-void literal::mk_constraint( const char * s )
+void literal::mk_constraint( const char * src )
 {
-    char * src = const_cast<char*> ( s );
     rp_parse_constraint_string ( _c , src, (*_ts) );
 }
 
-void infix( Enode *e, ostream & os )
+void infix( const Enode *e, ostream & os )
 {
   Enode *p = NULL;
-  if( e->isSymb( ) )
+  if( e->isSymb( ) ) {
     os << e->getName( );
+  }
   else if ( e->isNumb( ) )
   {
     os << e->getName( );
   }
   else if ( e->isTerm( ) )
   {
-    if ( !e->getCdr()->isEnil( ) )
+    // output "("
+    if ( !e->getCdr()->isEnil( ) && ( e->isPlus() || e->isMinus() || e->isTimes() || e->isPow() ) ) {
       os << "(";
-
-    infix(e->getCar(), os);
-
-    p = e->getCdr();
-    while ( !p->isEnil( ) )
-    {
-      os << " ";
-      infix(p->getCar(), os);
-      p = p->getCdr();
     }
 
-    if ( !e->getCdr()->isEnil( ) )
-      os << ")";
-  }
-  else if ( e->isList( ) )
-  {
-    if ( e->isEnil( ) )
-      os << "-";
-    else
-    {
-      os << "[";
-      infix(e->getCar(), os);
+    if ( e->isPlus() ||
+         e->isMinus() ||
+         e->isTimes() ||
+         e->isDiv() ||
+         e->isEq() ||
+         e->isLeq() ||
+         e->isGeq() ||
+         e->isLt() ||
+         e->isGt() ||
+         e->isPow()
+         )
+      {
+        assert(e->getArity() == 2);
 
+        // output 1st argument
+        infix(e->getCdr()->getCar(), os);
+        os << " ";
+        // output operator
+        infix(e->getCar(), os);
+        os << " ";
+        // output 2nd argument
+        infix(e->getCdr()->getCdr()->getCar(), os);
+      }
+    else if (e->isArcCos() ||
+             e->isArcSin() ||
+             e->isArcTan() ||
+             e->isSin() ||
+             e->isCos() ||
+             e->isTan() ||
+             e->isLog() ||
+             e->isExp()
+             ) {
+        assert(e->getArity() == 1);
+        // output operator
+        infix(e->getCar(), os);
+        os << "(";
+        // output 1st argument
+        infix(e->getCdr()->getCar(), os);
+        os << ")";
+    }
+    else {
+      infix(e->getCar(), os);
       p = e->getCdr();
       while ( !p->isEnil( ) )
-      {
-	os << ", ";
-	infix(p->getCar(), os);
-	p = p->getCdr();
-      }
+        {
+          os << " ";
+          // print Car
+          infix(p->getCar(), os);
+          p = p->getCdr();
+        }
+    }
 
-      os << "]";
+    // output ")"
+    if ( !e->getCdr()->isEnil( ) && ( e->isPlus() || e->isMinus() || e->isTimes() || e->isPow() )) {
+      os << ")";
     }
   }
+  else if ( e->isList( ) )
+    {
+      if ( e->isEnil( ) )
+        os << "-";
+      else
+        {
+          os << "[";
+          infix(e->getCar(), os);
+
+          p = e->getCdr();
+          while ( !p->isEnil( ) )
+            {
+              os << ", ";
+              infix(p->getCar(), os);
+              p = p->getCdr();
+            }
+
+          os << "]";
+        }
+    }
   else if ( e->isDef( ) )
-  {
-    os << ":= " << e->getCar();
-  }
+    {
+      os << ":= " << e->getCar();
+    }
   else if ( e->isEnil( ) )
-  {
-    os << "-";
-  }
+    {
+      os << "-";
+    }
   else
     opensmt_error( "unknown case value" );
 }
 
-const char * infix(Enode *e)
+const string infix(const Enode *e)
 {
   stringstream buf;
   infix(e, buf);
-  return buf.str().c_str();
+  return buf.str();
 }
--- src/literal.h
+++ src/literal.h
@@ -8,6 +8,7 @@ public:
 
 	inline Enode * 	get_enode()	{ return _e; }
 	void		mk_constraint( const char * );
+	rp_constraint *		_c;
 
 private:
 
@@ -15,9 +16,8 @@ private:
 	lbool polarity;	//whether it's negated
 
 	Enode *			_e;	//original enode position
-	rp_constraint *		_c;
 	rp_table_symbol * 	_ts; //pointer to an outside symbol table
 
 };
 
-const char * infix(Enode *e);
+const string infix(const Enode *e);
