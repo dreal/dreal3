6891750805cdc50b3b6d4e92298b0717012697e1
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-15 23:46:13 -0500

    smt2: add infix printer

--- tools/bmc/dr.ml
+++ tools/bmc/dr.ml
@@ -281,3 +281,77 @@ let print out ((vardecls, odes, f) : t) : unit =
     (* print variable declarations *)
     print_formula out f
   end
+
+let rec print_infix_exp (out : 'a BatInnerIO.output) : exp -> unit =
+  let print_infix_exps (op : string) (exps : exp list) =
+    begin
+      BatList.print
+        (~first:"(")
+        (~sep: (" " ^ op ^ " "))
+        (~last:")")
+        print_infix_exp
+        out
+        exps
+    end
+  in
+  function
+  | Var x ->
+    let filter str =
+      (* filter out '(' and ')' *)
+      let s1 = BatString.filter (fun c -> c != '(' && c != ')') str in
+      (* replace '*' with "ptr_" *)
+      let s2 = BatString.replace_chars (fun c -> if c == '*' then "ptr_" else BatString.of_char c) s1 in
+      (* replace '.' with "_" *)
+      let s3 = BatString.replace_chars (fun c -> if c == '.' then "_" else BatString.of_char c) s2 in
+      (* replace "->" with "_" *)
+      let rec replace_all (str : string) (sub : string) (by : string) =
+        let (b, str') = BatString.replace str sub by in
+        match b with
+        | true -> replace_all str' sub by
+        | false -> str'
+      in
+      replace_all s3 "->" "_"
+    in
+    BatString.print out (filter x)
+  | Const n ->
+    (* If n ends with ".", add "0" to make ".0" *)
+    let s = BatFloat.to_string n in
+    let s' = match BatString.ends_with s "." with
+      | true -> s ^ "0"
+      | false -> s
+    in
+    BatString.print out s'
+  | Neg e' -> print_infix_exps "-" [e']
+  | Add (e1, e2) -> print_infix_exps "+" [e1; e2]
+  | Sub (e1, e2) -> print_infix_exps "-" [e1; e2]
+  | Mul (e1, e2) -> print_infix_exps "*" [e1; e2]
+  | Div (e1, e2) -> print_infix_exps "/" [e1; e2]
+  | Pow (e1, e2) -> print_infix_exps "^" [e1; e2]
+  | Ite (f, e1, e2) ->
+    begin
+      BatString.print out "(ite ";
+      print_formula out f;
+      BatString.print out " ";
+      print_infix_exp out e1;
+      BatString.print out " ";
+      print_infix_exp out e2;
+      BatString.print out ")"
+    end
+  | Sqrt e -> (* print_infix_exps "sqrt" [e] *)
+    (* MATH HACK *)
+    print_infix_exp out (Pow(e, Const 0.5))
+  | Abs  e -> (* print_infix_exps "abs"  [e] *)
+    (* MATH HACK *)
+    print_infix_exp out (Sqrt (Pow(e, Const 2.0)))
+  | Log  e -> print_infix_exps "log"  [e]
+  | Exp  e -> print_infix_exps "exp"  [e]
+  | Sin  e -> print_infix_exps "sin"  [e]
+  | Cos  e -> print_infix_exps "cos"  [e]
+  | Tan  e -> print_infix_exps "tan"  [e]
+  | Asin e -> print_infix_exps "arcsin" [e]
+  | Acos e -> print_infix_exps "arccos" [e]
+  | Atan e -> print_infix_exps "arctan" [e]
+  | Sinh e -> print_infix_exps "sinh" [e]
+  | Cosh e -> print_infix_exps "cosh" [e]
+  | Tanh e -> print_infix_exps "tanh" [e]
+
