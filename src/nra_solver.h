cc409f9a0f0825b5f3a5b906532b3480c515d524
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-17 11:40:56 -0500

    clear up NRASolver

--- src/nra_solver.h
+++ src/nra_solver.h
@@ -3,22 +3,20 @@
 
 #include "TSolver.h"
 #include "Egraph.h"
-#include "icp_solver.h"
-#include "variable.h"
 
 class NRASolver : public OrdinaryTSolver
 {
 public:
 
-    NRASolver( const int, const char *, SMTConfig &, Egraph &, SStore &
-               , vector< Enode * > &
-               , vector< Enode * > &
-               , vector< Enode * > &);
-
+    NRASolver( const int,
+               const char *,
+               SMTConfig &,
+               Egraph &,
+               SStore &,
+               vector< Enode * > &,
+               vector< Enode * > &,
+               vector< Enode * > &);
     ~NRASolver ( );
-
-    bool            icp_solve		        ( set<variable*> );
-    bool            icp_prop                    ( set<variable*> );
 
     lbool  	    inform              	( Enode * );
     bool            assertLit           	( Enode *, bool = false );
@@ -28,15 +26,7 @@ public:
     bool            belongsToT          	( Enode * );
     void            computeModel        	( );
 
-    set<variable *> get_variables       (Enode * e);
-    variable *	    create_variable	( Enode * );
-
-    void	    add_literal	        ( Enode * );
-    void	    pop_literal	        ( vector<literal*>::size_type );
-
 private:
-    vector < variable * >	              v_set;
-    vector < literal *  >	              asserted_lits;
-    vector < vector< pair<double, double> > > history;
+
 };
 #endif
