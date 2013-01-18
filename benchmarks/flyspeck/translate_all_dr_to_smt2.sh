646490e1598d8afa69bc569b2182b7027c2b392c
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-17 20:06:22 -0500

    update translate script to take in options

--- benchmarks/flyspeck/translate_all_dr_to_smt2.sh
+++ benchmarks/flyspeck/translate_all_dr_to_smt2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 TRANSLATOR=../../tools/dr2smt2/dr2smt2.native
+TRANSLATOR_OPT=-f
 
 for DR in `ls -1 *.dr`
 do
 	SMT=${DR//dr/smt2}
 	echo converting $DR to $SMT
-	time $TRANSLATOR $DR > $SMT
+	time $TRANSLATOR $TRANSLATOR_OPT $DR > $SMT
 done
