f9794baf1c114ba0192316c971eb67e22a7b62a6
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-17 19:32:42 -0500

    implement icp_solver from scratch

--- src/icp_solver.cpp
+++ src/icp_solver.cpp
@@ -3,87 +3,193 @@
 #include "icp_solver.h"
 using namespace std;
 
-#define is_null(x) (x == NULL ? "NULL" : "Non-Null")
-
-icp_solver::icp_solver(rp_problem * p,
-                       set < variable* > ode_vars,
-                       double improve,
-                       rp_selector * vs,
-                       rp_splitter * ds,
-                       rp_existence_prover * ep
-    ):
-        _problem(p),
-        _ode_vars(ode_vars),
-        _propag(p),
-        _boxes(rp_problem_nvar(*p)), //number of variables
-        _vselect(vs),
-        _dsplit(ds),
-        _ep(ep),
-        _sol(0),
-        _nsplit(0)
+icp_solver::icp_solver(const vector<Enode*> & stack,
+                       map<Enode*, pair<double, double> > & env,
+                       vector<Enode*> & exp,
+                       double improve
+    )
+    :
+    _stack(stack),
+    _env(env),
+    _boxes(env.size()), //number of variables
+    _propag(NULL),
+    _ep(NULL),
+    _sol(0),
+    _nsplit(0),
+    _explanation(exp)
 {
-        // Check once the satisfiability of all the constraints
-        // Necessary for variable-free constraints
-        int i = 0, sat = 1;
+    rp_init_library();
+    _problem = create_rp_problem(stack, env);
 
-        while ((i<rp_problem_nctr(*p)) && (sat))
-        {
-                if (rp_constraint_unfeasible(rp_problem_ctr(*p,i),rp_problem_box(*p)))
-                {
-                        sat = 0;
-                }
-                else ++i;
-        }
+    rp_problem_display(stdout, *_problem);
 
-        if (sat)
-        {
-                // Insertion of the initial box in the search structure
-                _boxes.insert(rp_problem_box(*p));
+    // _propag = new rp_propagator(_problem);
 
-                // Management of improvement factor
-                if ((improve>=0.0) && (improve<=100.0))
-                {
-                        _improve = 1.0-improve/100.0;
-                }
-                else
-                {
-                        _improve = 0.875;  /* 12.5% */
-                }
-                _propag.set_improve(_improve);
+    rp_selector * _vselect;
+    rp_new( _vselect, rp_selector_roundrobin, (_problem) );
 
-                // Creation of the operators and insertion in the propagator
-                rp_hybrid_factory hfact(_improve);
-                hfact.apply(p,_propag);
+    rp_splitter * _dsplit;
+    rp_new( _dsplit, rp_splitter_mixed, (_problem) );
 
-                rp_domain_factory dfact;
-                dfact.apply(p,_propag);
+    solver = new rp_bpsolver(_problem,improve,_vselect,_dsplit); //,prover);
 
-		cerr << "icp_solver::icp_solver" << endl;
-		cerr << rp_problem_nctr(*p) << endl;
-		rp_problem_display(stdout, *p);
+    // // Check once the satisfiability of all the constraints
+    // // Necessary for variable-free constraints
+    // int i = 0, sat = 1;
+
+    // while ((i<rp_problem_nctr(*_problem)) && (sat))
+    // {
+    //     if (rp_constraint_unfeasible(rp_problem_ctr(*_problem,i),rp_problem_box(*_problem)))
+    //     {
+    //         sat = 0;
+    //     }
+    //     else ++i;
+    // }
+
+    // if (sat)
+    // {
+    //     // Insertion of the initial box in the search structure
+    //     _boxes.insert(rp_problem_box(*_problem));
+
+    //     // Management of improvement factor
+    //     if ((improve>=0.0) && (improve<=100.0))
+    //     {
+    //         _improve = 1.0-improve/100.0;
+    //     }
+    //     else
+    //     {
+    //         _improve = 0.875;  /* 12.5% */
+    //     }
+    //     _propag->set_improve(_improve);
+
+    //     // Creation of the operators and insertion in the propagator
+    //     rp_hybrid_factory hfact(_improve);
+    //     hfact.apply(_problem,*_propag);
+
+    //     rp_domain_factory dfact;
+    //     dfact.apply(_problem,*_propag);
+
+    //     cerr << "icp_solver::icp_solver" << endl;
+    //     cerr << rp_problem_nctr(*_problem) << endl;
+    //     rp_problem_display(stdout, *_problem);
 
 
-                rp_newton_factory nfact(_improve);
-                nfact.apply(p,_propag);
+    //     rp_newton_factory nfact(_improve);
+    //     nfact.apply(_problem,*_propag);
 
-                //rp_3b_factory bfact(_improve);
-                //bfact.apply(p,_propag);
+    //     //rp_3b_factory bfact(_improve);
+    //     //bfact.apply(_problem,_propag);
 
-                // Used for round-robin strategy: last variable split
-                rp_box_set_split(_boxes.get(),-1);//-1?: length - this number
-        }
-        else
-        {
-                rp_box_set_empty(rp_problem_box(*p));
-        }
+    //     // Used for round-robin strategy: last variable split
+    //     rp_box_set_split(_boxes.get(),-1);//-1?: length - this number
+    // }
+    // else
+    // {
+    //     rp_box_set_empty(rp_problem_box(*_problem));
+    // }
+
+    cerr << "icp_solver::icp_solver() End." << endl;
 }
 
+rp_problem* icp_solver::create_rp_problem(const vector<Enode*> & stack,
+                                          map<Enode*, pair<double, double> > & env)
+{
+    cerr << "icp_solver::create_rp_problem" << endl;
+
+    rp_problem* result = new rp_problem;
+    rp_problem_create( result, "icp_holder" );
+
+    // ======================================
+    // Create rp_variable for each var in env
+    // ======================================
+    for(map<Enode*, pair<double, double> >::const_iterator ite = env.begin();
+        ite != env.end();
+        ite++)
+    {
+        Enode* key = (*ite).first;
+        double lb =  (*ite).second.first;
+        double ub =  (*ite).second.second;
+
+        rp_variable * _v = new rp_variable;
+        string name = key->getCar()->getName();
+        rp_variable_create( _v, name.c_str());
+
+        int rp_id = rp_vector_insert(rp_table_symbol_vars(rp_problem_symb(*result)),
+                                     (*_v));
+
+        rp_box_enlarge_size(&rp_problem_box (*result), 1);
+
+        rp_bsup(rp_box_elem(rp_problem_box(*result), rp_id)) = ub;
+        rp_binf(rp_box_elem(rp_problem_box(*result), rp_id)) = lb ;
+
+        cerr << "["
+             << rp_bsup(rp_box_elem(rp_problem_box(*result), rp_id))
+             << ","
+             << rp_binf(rp_box_elem(rp_problem_box(*result), rp_id))
+             << "]" << endl;
+
+        rp_union_interval u;
+        rp_union_create(&u);
+        rp_union_insert(u,
+                        rp_box_elem(rp_problem_box(*result),
+                                       rp_id));
+        rp_union_copy(rp_variable_domain(*_v),u);
+        rp_union_destroy(&u);
+
+        enode_to_rp_id[key] = rp_id;
+
+        cerr << "Key: " << name << "\t"
+             << "value : [" << lb << ", " << ub << "] \t"
+             << "rp_id: " << rp_id << endl;
+    }
+
+    // ===============================================
+    // Create rp_constraints for each literal in stack
+    // ===============================================
+    for(vector<Enode*>::const_iterator ite = stack.begin();
+        ite != stack.end();
+        ite++)
+    {
+        Enode* l = *ite;
+        stringstream buf;
+        rp_constraint * _c = new rp_constraint;
+        l->print_infix(buf, l->getPolarity());
+        string temp_string = buf.str();
+
+        if(temp_string.compare("0 = 0") != 0) {
+            // Parse the string (infix form) to create the constraint _c
+            rp_parse_constraint_string ( _c ,
+                                         temp_string.c_str(),
+                                         rp_problem_symb(*result) );
+
+            // Add to the problem
+            rp_vector_insert(rp_problem_ctrs(*result),
+                             *_c);
+
+            // Update Counter
+            for (int i=0; i < rp_constraint_arity(*_c); ++i)
+            {
+                ++rp_variable_constrained(rp_problem_var(*result,rp_constraint_var(*_c,i)));
+            }
+
+            cerr << "Constraint: "
+                 << (l->getPolarity() == l_True ? " " : "Not")
+                 << l << endl;
+            cerr << "          : " << temp_string << endl;
+        }
+    }
+    return result;
+}
 
 icp_solver::~icp_solver()
 {
-	rp_delete(_vselect);
-	rp_delete(_dsplit);
-	if (_ep) rp_delete(_ep);
+//    rp_delete(_vselect);
+//    rp_delete(_dsplit);
+//    if (_ep) rp_delete(_ep);
+    rp_problem_destroy(_problem);
+    rp_reset_library();
+//    delete _propag;
+    delete solver;
 }
 
 rp_box icp_solver::prop()
@@ -91,72 +197,91 @@ rp_box icp_solver::prop()
   cerr << "icp_solver::prop" << endl;
   assert(_boxes.size() == 1);
 
-  _propag.apply(_boxes.get());
+  _propag->apply(_boxes.get());
   return( _boxes.get() );
 }
 
-bool icp_solver::propagation_with_ode (rp_box b, bool hasDiff)
-{
-    if(_propag.apply(b))
-    {
-        if (hasDiff)
-        {
-            rp_box current_box = _boxes.get();
 
-            // Partition _ode_vars into subsets by their diff_group
-            int max = 1;
-            vector< set< variable* > > diff_vec(max);
-            for(set<variable*>::iterator ite = _ode_vars.begin();
-                ite != _ode_vars.end();
+bool icp_solver::solve()
+{
+    //rp_problem_display(stdout,problem);
+
+    if (rp_box_empty(rp_problem_box(*_problem)))
+    {
+        cerr << "Unfeasibility detected before solving";
+
+        /* TODO: what about explanation? */
+        copy(_stack.begin(),
+             _stack.end(),
+             std::back_inserter(_explanation));
+
+        return false;
+    }
+    else
+    {
+        // rp_ofilter_text oft(&problem,&cout,-1);
+        // rp_ofilter_pstricks oft(&problem,&cout);
+        // oft.apply_box(rp_problem_box(problem),"[initial]");
+        // int clock_solve = rp_clock_create();
+        // rp_clock_start(clock_solve);
+
+        rp_box b;
+//        int nb_isafe = 0;
+        if ((b=solver->compute_next())!=NULL)
+        {
+            /* SAT */
+            // cerr << rp_clock_elapsed_time(clock_solve) << "ms" << endl;
+//            if (rp_box_interval_safe(b)) ++nb_isafe;
+            cerr << "SAT with the following box:" << endl;
+            rp_box_display_simple(b);
+
+            // Update the value in the env
+            cerr << "Update ENV!!!!!!!!!" << endl;
+            for(map<Enode*, pair<double, double> >::const_iterator ite = _env.begin();
+                ite != _env.end();
                 ite++)
             {
-                int diff_group = (*ite)->get_enode()->getODEgroup();
-                cerr << "diff_group: " << diff_group << ", max: " << max << endl;
-                if(diff_group >= max) {
-                    cerr << "diff_group: " << diff_group << " we do resize" << endl;
-                    diff_vec.resize(diff_group + 1);
-                    max = diff_group;
-                    cerr << "max: " << max << endl;
-                }
-                if(diff_vec[diff_group].empty())
-                    cerr << "diff_vec[" << diff_group << "] is empty!!" << endl;
-                diff_vec[diff_group].insert(*ite);
-                cerr << "diff_group inserted: " << diff_group << endl;
+                Enode* key = (*ite).first;
+                int rp_id = enode_to_rp_id[key];
+                double ub = rp_bsup(rp_box_elem(rp_problem_box(*_problem), rp_id));
+                double lb = rp_binf(rp_box_elem(rp_problem_box(*_problem), rp_id));
+                _env[key] = make_pair(lb, ub);
+                cout << "Key: " << key << "\t Value: [" << lb << ", " << ub << "]" << endl;
             }
 
-            for(int i = 1; i <= max; i++)
-            {
-                cerr << "solve ode group: " << i << endl;
-                set<variable*> current_ode_vars = diff_vec[i];
 
-                if(!current_ode_vars.empty()) {
-                    cerr << "Inside of current ODEs" << endl;
-                    for(set<variable*>::iterator ite = current_ode_vars.begin();
-                        ite != current_ode_vars.end();
-                        ite++)
-                    {
-                        cerr << "Name: " << (*ite)->getName() << endl;
-                        (*ite)->set_top_box(&current_box);
-                    }
-
-                    (*current_ode_vars.begin())->getODEtimevar()->set_top_box(&current_box);
-                    ode_solver odeSolver(current_ode_vars);
-                    if (!odeSolver.solve())
-                        return false;
-                }
-            }
             return true;
         }
         else {
-            return true;
+            /* UNSAT */
+
+            /* TODO: what about explanation? */
+            cerr << "UNSAT!" << endl;
+            copy(_stack.begin(),
+                 _stack.end(),
+                 std::back_inserter(_explanation));
+
+            return false;
         }
+        // rp_clock_stop(clock_solve);
+        // cout << std::endl
+        //      << "Solving in "
+        //      << rp_clock_get(clock_solve) << "ms"
+        //      << std::endl
+        //      << solver.solution()
+        //      << " solution(s)"
+        //      << std::endl
+        //      << solver.nsplit()
+        //      << " split(s)"
+        //      << std::endl
+        //      << solver.nboxes()
+        //      << " box(es) created in memory"
+        //      << std::endl;
     }
-    return false;
 }
 
 rp_box icp_solver::compute_next(bool hasDiff)
 {
-
     cout<<"------------------"<<endl;
     cout<<"The interval pruning and branching trace is:";
 
@@ -170,7 +295,7 @@ rp_box icp_solver::compute_next(bool hasDiff)
           cout<<endl<<"[before pruning] "<<endl;
           rp_box_cout(_boxes.get(), 5, RP_INTERVAL_MODE_BOUND );
         */
-        if (propagation_with_ode(_boxes.get(), hasDiff))
+        if (_propag->apply(_boxes.get()))
         {
             int i;
             if ((i=_vselect->apply(_boxes.get()))>=0)
@@ -218,4 +343,3 @@ icp_solver& icp_solver::operator=(const icp_solver& s)
 {
   	return( *this );
 }
-
--- src/icp_solver.h
+++ src/icp_solver.h
@@ -2,46 +2,56 @@
 #ifndef ICPSOLVER_H
 #define ICPSOLVER_H
 
-#include "literal.h"
-#include "ode_solver.h"
+#include "realpaver.h"
+#include "Enode.h"
 
 class icp_solver
 {
 public:
+    icp_solver(const vector<Enode*> & stack,
+               map<Enode*, pair<double, double> > & env,
+               vector<Enode*> & exp,
+               double improve
+              );
 
-  	icp_solver( rp_problem * p,
-                    set < variable* > ode_vars,
-                    double improve, rp_selector * vs,
-                    rp_splitter * ds, rp_existence_prover * ep = 0);
+    ~icp_solver();
 
-  	~icp_solver();
+    rp_problem*   create_rp_problem(const vector<Enode*> & stack,
+                                    map<Enode*, pair<double, double> > & env);
 
-        bool            propagation_with_ode (rp_box , bool );
-  	rp_box 		compute_next(bool hasDiff); //computation of the next solution
-        rp_box          prop();          //only propagate
+    rp_box        compute_next(bool hasDiff); //computation of the next solution
+    rp_box        prop();         //only propagate
 
-  	int 		solution();	//number of solutions
-  	int 		nboxes();	//number of boxes
-  	int 		nsplit();	//number of branching
+    int           solution();     //number of solutions
+    int           nboxes();       //number of boxes
+    int           nsplit();       //number of branching
 
-        void           	prune                   ( rp_box *, literal * );
-        void            prune                   ( rp_box *, rp_problem * );
-        void            branch                  ( rp_box * );
+    bool          solve();
+
+    /* void          prune                   ( rp_box *, literal * ); */
+    /* void          prune                   ( rp_box *, rp_problem * ); */
+    /* void          branch                  ( rp_box * ); */
 
 private:
 
-  	rp_problem * _problem;     /* problem to be solved                    */
-  	rp_propagator _propag;     /* reduction algorithm using propagation   */
-  	rp_box_stack _boxes;       /* the set of boxes during search          */
-  	rp_selector * _vselect;    /* selection of variable to be split       */
-  	rp_splitter * _dsplit;     /* split function of variable domain       */
-  	rp_existence_prover * _ep; /* existence prover                        */
-        set < variable* > _ode_vars;
-  	int _sol;                  /* number of computed solutions            */
-  	int _nsplit;               /* number of split steps                   */
-  	double _improve;           /* improvement factor of iterative methods */
+    rp_problem * _problem;     /* problem to be solved                    */
+    rp_propagator * _propag;     /* reduction algorithm using propagation   */
+    rp_box_stack _boxes;       /* the set of boxes during search          */
+    rp_selector * _vselect;    /* selection of variable to be split       */
+    rp_splitter * _dsplit;     /* split function of variable domain       */
+    rp_existence_prover * _ep; /* existence prover                        */
+    int _sol;                  /* number of computed solutions            */
+    int _nsplit;               /* number of split steps                   */
+    double _improve;           /* improvement factor of iterative methods */
 
-        icp_solver& operator=(const icp_solver& s);
+    map<Enode*, int> enode_to_rp_id;
+    rp_bpsolver * solver;
+
+    vector<Enode*> & _explanation;
+    const vector<Enode*> & _stack;
+    map<Enode*, pair<double, double> > & _env;
+
+    icp_solver& operator=(const icp_solver& s);
 };
 
 #endif
