c84af16afbd5b757f0f53ca0e60dc5c4a46ed8ed
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-20 00:30:10 -0500

    kepler>script>split.py: do nothing if there is only one trace to make

--- tools/kepler/script/split.py
+++ tools/kepler/script/split.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 import sys
 import os
+import shutil
+
 trace_name = sys.argv[1]
 base_name, ext = os.path.splitext(trace_name)
 input = open(trace_name)
 counter = 0
+
+def touch(fname, times=None):
+    with file(fname, 'a'):
+        os.utime(fname, times)
 
 def get_newname():
     global counter
@@ -14,7 +20,15 @@ def get_newname():
 # Create a init out
 output = sys.stdout
 
+print "Split " + trace_name,
+
 for line in input:
     if line.startswith("Precision:"):
         output = open(get_newname(), "w")
     output.write(line)
+
+print " into " + str(counter) + " traces."
+
+if counter == 1:
+    touch(trace_name)
+    os.remove(base_name + "_1.trace")
