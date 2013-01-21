35b244786731e571cbb82c51e93acafa8eee1cc1
Soonho Kong <soonhok@cs.cmu.edu>
2013-01-21 16:50:26 -0500

    update run.sh

--- tools/kepler/script/run.sh
+++ tools/kepler/script/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 INEQDIR=$1
 RESULTDIR=$2
-MAX=1
+MAX=30
+RUN_SINGLE=~/work/dreal2/tools/kepler/script/run_single.sh
 
-find $INEQDIR -name "*.smt2" | grep -v "minismt" | parallel --max-procs=$MAX "echo {}; ./run_single.sh {} $RESULTDIR"
+find $INEQDIR -name "*.smt2" | grep -v "minismt" | parallel --max-procs=$MAX "echo {}; $RUN_SINGLE {} $RESULTDIR"
