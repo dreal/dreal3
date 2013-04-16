6e864a7fec3e2728c3505b5092abe351dfdc3e04
Soonho Kong <soonhok@cs.cmu.edu>
2013-04-16 16:08:11 -0400

    add bmc.sh script, which preprocess .drh and call ocaml bmc

---
+++ tools/bmc/bmc.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+SCRIPTPATH=`dirname $(readlink -f $0)`
+BMC=${SCRIPTPATH}/main.native
+
+#################################################################
+# USAGE
+#################################################################
+usage()
+{
+    cat << EOF
+usage: $0 options <*.drh>
+
+bmc: Bounded Model Checking for for Nonlinear Hybrid Systems
+
+OPTIONS:
+   -k      unrolling steps (default 3)
+EOF
+}
+
+#################################################################
+# Parse Option
+#################################################################
+K=3
+PARSER=
+while getopts "hk:i" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        k)
+            K=$OPTARG
+            ;;
+        i)
+            PARSER="--infix"
+            ;;
+        \?)
+            usage
+            exit
+            ;;
+    esac
+done
+
+#################################################################
+# Check main.native
+#################################################################
+if [ ! -e $BMC ]
+then
+    echo "BMC(main.native) is not found at $BMC"
+    echo "Please edit $0 to specify the correct location of BMC tool"
+    exit 1
+fi
+
+shift $(($OPTIND - 1))
+if [[ ! -e $1 || ! ${1: -4} == ".drh" ]]
+then
+    usage
+    exit 1
+fi
+
+function log_output {
+    echo `date`: "$1"
+}
+
+BASE=${1%.drh}
+DRH=$BASE.drh
+PDRH=$BASE.preprocessed.drh
+SMT=${BASE}_$K.smt2
+log_output "Preprocess $DRH > $PDRH"
+cpp -P -traditional-cpp $DRH > $PDRH
+log_output "$BMC -k $K $PARSER $PDRH > $SMT..."
+$BMC -k $K $PARSER $PDRH > $SMT
+log_output "done"
