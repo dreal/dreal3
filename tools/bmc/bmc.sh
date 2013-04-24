b42ecfd07cd6a9be32ceed6a5aa31989b3f74407
Soonho Kong <soonhok@cs.cmu.edu>
2013-04-24 14:53:41 -0400

    fix bmc.sh

--- tools/bmc/bmc.sh
+++ tools/bmc/bmc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPTPATH=`dirname $(readlink $0)`
+SCRIPTPATH=`dirname $(readlink -f $0)`
 BMC=${SCRIPTPATH}/main.native
 
 #################################################################
