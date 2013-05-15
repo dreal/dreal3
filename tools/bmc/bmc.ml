abb6cedf42e1e867d435b3ba3c5f7624ffc476c8
Soonho Kong <soonhok@cs.cmu.edu>
2013-05-14 21:18:40 -0400

    bmc: use new encoding for mode variable

--- tools/bmc/bmc.ml
+++ tools/bmc/bmc.ml
@@ -95,14 +95,7 @@ let process_jump (jump) (k : int) (next_k : int) (q : id) (next_q : id) (all_var
       | false -> add_index k q "_t" v)
       change'
   in
-  let mode_cond =
-    Basic.make_and
-      (List.map
-         (fun (k, q) ->
-           Basic.Eq (Basic.Var ("mode_" ^ (string_of_int k)), Basic.Num (float_of_int q)))
-         [(k, q); (next_k, next_q)])
-  in
-  Basic.make_and [gurad'; change''; mode_cond]
+  Basic.make_and [gurad'; change'']
 
 let process_goals (hm : Hybrid.t) (k : int) (goals : (int * formula) list) (all_vars : var Set.t) =
   let goal_formulas =
@@ -126,6 +119,8 @@ let varmap_to_list vardeclmap =
     vardeclmap
     []
 
+let make_mode_cond k q =
+  Basic.Eq (Basic.Var ("mode_" ^ (string_of_int k)), Basic.Num (float_of_int q))
 let trans modemap init_id (k : int) (next_k : int) (q : id) (next_q : id) (all_vars : var Set.t)
     : (flows_annot * formula)
     =
@@ -133,8 +128,11 @@ let trans modemap init_id (k : int) (next_k : int) (q : id) (next_q : id) (all_v
   let jump_result = process_jump (Jumpmap.find next_q (Mode.jumpmap m)) k next_k q next_q all_vars in
   let (flow_k_next_ode, flow_k_next) =
     process_flow (next_k) (next_q) (Modemap.find next_q modemap) in
+  let mode_cond = make_mode_cond k q in
+  let mode_cond_n = make_mode_cond next_k next_q in
   (flow_k_next_ode,
-   Basic.make_and [jump_result; flow_k_next])
+   Basic.Imply (mode_cond,
+                Basic.make_and [mode_cond_n; jump_result; flow_k_next]))
 
 let transform modemap init_id (k : int) (next_k : int) (edge_op : (int * int) option) (all_vars : var Set.t)
     : (flows_annot * formula)
@@ -173,7 +171,7 @@ let transform modemap init_id (k : int) (next_k : int) (edge_op : (int * int) op
       candidate_pairs'
   in
   let (flow_list, formula_list) = List.split trans_result_list in
-  (List.flatten flow_list, Basic.make_or formula_list)
+  (List.flatten flow_list, Basic.make_and formula_list)
 
 let reach (k : int) (hm : Hybrid.t) (path : int list option):
     (varDecl list * flows_annot * formula * Value.t)
