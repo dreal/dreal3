73644777e5bb77de39da2cd0fc1e9ce952be979f
Sean Gao <sicung@cs.cmu.edu>
2013-06-04 21:55:05 -0400

    starting dmacs

---
+++ dmacs/hybrid_systems.py
@@ -0,0 +1,18 @@
+# define the hybrid system data structure
+
+class Mode:
+	invt = []
+	flow = []
+	guard = [] 
+	reset = [] #list of pairs (to_mode, reset conditions)
+
+class HySys:
+	mode = []	
+
+def concat(h1,h2):
+	return
+
+def product(h1,h2):
+	return
+
+
