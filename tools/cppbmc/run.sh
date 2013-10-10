281897b64f485fd62328c2816283df69af9844dc
Sean Gao <sicung@cs.cmu.edu>
2013-10-10 15:24:52 -0400

    fix(tools): remove cppbmc

--- tools/cppbmc/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CFILE=$1
-BASE=${CFILE%%.c}
-IFILE=${BASE}.i
-GM/make_i.sh $1
-./pass1.native $IFILE
--- tools/cppbmc/test/test1.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-	double __attribute__ ((warning("3, 4"))) x = 3;
-	double y = 4;
-	x = y;
-	y = 10;
-	BMC_CHECK("FORMULA");
-	return 10;
-}
