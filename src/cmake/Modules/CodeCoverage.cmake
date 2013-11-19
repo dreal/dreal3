9c5e796a8358daed8c21e3059580baf8144798d5
Soonho Kong <soonhok@cs.cmu.edu>
2013-11-19 00:17:47 -0500

    chore(cmake): add test coverage

--- src/cmake/Modules/CodeCoverage.cmake
+++ src/cmake/Modules/CodeCoverage.cmake
@@ -92,9 +92,9 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
                 # Capturing lcov counters and generating report
                 COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-                COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'tests/*' '/usr/*' --output-file ${_outputname}.info.cleaned
+                COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'tests/*' '/usr/*' 'src/realpaver/*' 'src/opensmt/*' 'build/*' --output-file ${_outputname}.info.cleaned
                 COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
-                COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
+                COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info
 
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
