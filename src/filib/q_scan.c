5d89604c1a963278ecce9bccbad21ce5b893c7fa
Soonho Kong <soonhok@cs.cmu.edu>
2013-11-20 21:30:54 -0500

    fix(filib/q_scan.c): unused return value warnings

--- src/filib/q_scan.c
+++ src/filib/q_scan.c
@@ -3,14 +3,15 @@
 /*        (For copyright and info`s see file "fi_lib.h")             */
 /*********************************************************************/
 
-#include "fi_lib.h" 
-
-
+#include <stdio.h>
+#include "fi_lib.h"
 
 double scandown()
 {
   double x;
-  scanf("%lf",&x);
+  if (scanf("%lf",&x) != 0) {
+      fprintf(stderr, "filib - q_scan.c::scanf failed\n");
+  }
   if (fabs(x)>(1e17-1)*1e27) return q_pred(q_pred(x));
   else return q_pred(x);
 }
@@ -18,13 +19,15 @@ double scandown()
 double scanup()
 {
   double x;
-  scanf("%lf",&x);
+  if (scanf("%lf",&x) != 0) {
+      fprintf(stderr, "filib - q_scan.c::scanf failed\n");
+  }
   if (fabs(x)>(1e17-1)*1e27) return q_succ(q_succ(x));
   else return q_succ(x);
 }
 
 interval scanInterval()
-{ 
+{
   interval dummy;
   dummy.INF = scandown();
   dummy.SUP = scanup();
