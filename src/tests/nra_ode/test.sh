7d8869ed672af9684ffd56b01209e64c3e87039b
Soonho Kong <soonhok@cs.cmu.edu>
2013-12-06 01:25:54 -0500

    test(nra_ode): use "option" files to run nra_ode tests

--- src/tests/nra_ode/test.sh
+++ src/tests/nra_ode/test.sh
@@ -2,9 +2,15 @@
 DREAL=$1
 INSTANCE=$2
 EXPECTED_OUT=$2.expected
+OPTION=
+if [[ -e $2.option ]]
+then
+    OPTION=`cat $2.option`
+fi
 TMP=$2.out
 
-$DREAL --ode-grid=1024 $INSTANCE | tee $TMP
+echo $DREAL $OPTION $INSTANCE
+$DREAL $OPTION $INSTANCE | tee $TMP
 diff $TMP $EXPECTED_OUT
 RESULT=$?
 rm $TMP
