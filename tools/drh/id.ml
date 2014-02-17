5e3ccfed912efa02cb6b999652c4d06f83e47348
Soonho Kong <soonhok@cs.cmu.edu>
2014-02-17 13:55:59 -0500

    feat(tools/drh): remove drh

--- tools/drh/id.ml
+++
@@ -1,8 +0,0 @@
-(*
- * Soonho Kong (soonhok@cs.cmu.edu)
- *)
-
-open Batteries
-
-type t = int
-let print = Int.print
--- tools/drh/vardecl.ml
+++
@@ -1,8 +0,0 @@
-open Batteries
-
-type value = Value.t
-type var = string
-type t = var * value
-
-let print out (var, value) =
-  Printf.fprintf out "%s %s" (IO.to_string Value.print value) var
