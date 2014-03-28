e4f666cc429d23e9ac655171824d67bf8362dac0
Soonho Kong <soonhok@cs.cmu.edu>
2014-03-28 12:15:12 -0400

    feat(tools/DSL): refactor drh_stl module, add more C math funcs

--- tools/DSL/src/drh_stl.ml
+++ tools/DSL/src/drh_stl.ml
@@ -1,51 +1,48 @@
 open Batteries
 open Cil
 
-let (libs : (string, Cil.varinfo) Map.t ref) = ref (Map.empty)
-(* why need type annotation? *)
+let libs : (string, Cil.varinfo) Map.t ref = ref (Map.empty)
+
+let add_math_func (fn_name : string) (arg_tys : Cil.typ list) (ret_ty : Cil.typ)
+    : unit =
+  let fn = emptyFunction fn_name in
+  let arg_postfixes = List.range 0 `To (List.length arg_tys - 1) in
+  let arg_names = List.map (fun n -> "x_" ^ (Int.to_string n)) arg_postfixes in
+  let args = List.map2 (fun name ty -> (name, ty, [])) arg_names arg_tys in
+  let args_opt = match arg_tys with
+    | [] -> None
+    | _ -> Some args in
+  let fn_ty = TFun (ret_ty, args_opt, false, []) in
+  let arg_varinfos =
+    List.map2 (fun name ty -> makeFormalVar fn name ty)
+      arg_names
+      arg_tys
+  in
+  let _ = setFormals fn arg_varinfos in
+  let _ = setFunctionType fn fn_ty in
+  libs := Map.add fn_name fn.svar !libs
+
+let math_funcs = [("sin", [Cil.doubleType], Cil.doubleType);
+                  ("cos", [Cil.doubleType], Cil.doubleType);
+                  ("tan", [Cil.doubleType], Cil.doubleType);
+                  ("acos", [Cil.doubleType], Cil.doubleType);
+                  ("asin", [Cil.doubleType], Cil.doubleType);
+                  ("atan", [Cil.doubleType], Cil.doubleType);
+                  ("atan2", [Cil.doubleType; Cil.doubleType], Cil.doubleType);
+                  ("cosh", [Cil.doubleType], Cil.doubleType);
+                  ("sinh", [Cil.doubleType], Cil.doubleType);
+                  ("tanh", [Cil.doubleType], Cil.doubleType);
+                  ("abs", [Cil.doubleType], Cil.doubleType);
+                  ("exp", [Cil.doubleType], Cil.doubleType);
+                  ("exp2", [Cil.doubleType], Cil.doubleType);
+                  ("pow", [Cil.doubleType; Cil.doubleType], Cil.doubleType);
+                  ("sqrt", [Cil.doubleType], Cil.doubleType);
+                  ("log", [Cil.doubleType], Cil.doubleType);
+                  ("log2", [Cil.doubleType], Cil.doubleType);
+                  ("log10", [Cil.doubleType], Cil.doubleType);
+                 ]
 
 let drh_stl_init () =
-  (* sin *)
-  let fn = "sin" in
-  let sin = emptyFunction fn in
-  let sinty = TFun (Cil.doubleType, Some ([("x", Cil.doubleType, [])]), false, []) in
-  let arg = makeFormalVar sin "x" (Formatcil.cType "double" []) in
-  let _ = setFormals sin [arg] in
-  let _ = setFunctionType sin sinty in
-  libs := Map.add fn sin.svar !libs;
-
-  (* cos *)
-  let fn = "cos" in
-  let sin = emptyFunction fn in
-  let sinty = TFun (Cil.doubleType, Some ([("x", Cil.doubleType, [])]), false, []) in
-  let arg = makeFormalVar sin "x" (Formatcil.cType "double" []) in
-  let _ = setFormals sin [arg] in
-  let _ = setFunctionType sin sinty in
-  libs := Map.add fn sin.svar !libs;
-
-  (* Tan *)
-  let fn = "tan" in
-  let sin = emptyFunction fn in
-  let sinty = TFun (Cil.doubleType, Some ([("x", Cil.doubleType, [])]), false, []) in
-  let arg = makeFormalVar sin "x" (Formatcil.cType "double" []) in
-  let _ = setFormals sin [arg] in
-  let _ = setFunctionType sin sinty in
-  libs := Map.add fn sin.svar !libs;
-
-  (* asin *)
-  let fn = "asin" in
-  let sin = emptyFunction fn in
-  let sinty = TFun (Cil.doubleType, Some ([("x", Cil.doubleType, [])]), false, []) in
-  let arg = makeFormalVar sin "x" (Formatcil.cType "double" []) in
-  let _ = setFormals sin [arg] in
-  let _ = setFunctionType sin sinty in
-  libs := Map.add fn sin.svar !libs;
-
-  (* acos *)
-  let fn = "acos" in
-  let sin = emptyFunction fn in
-  let sinty = TFun (Cil.doubleType, Some ([("x", Cil.doubleType, [])]), false, []) in
-  let arg = makeFormalVar sin "x" (Formatcil.cType "double" []) in
-  let _ = setFormals sin [arg] in
-  let _ = setFunctionType sin sinty in
-  libs := Map.add fn sin.svar !libs;
+  List.iter
+    (fun (name, arg_tys, ret_ty) -> add_math_func name arg_tys ret_ty)
+    math_funcs
