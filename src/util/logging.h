cbf8bc764210b62b12bae3b240d219d808d29313
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-17 17:50:15 -0400

    feat(util/logging): add DREAL_LOG*_IS_ON macro

--- src/util/logging.h
+++ src/util/logging.h
@@ -19,15 +19,21 @@ You should have received a copy of the GNU General Public License
 along with dReal. If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
-// FATAL - 0
-// ERROR - 1
-// WARNING - 2
-// INFO - 3
 #pragma once
 #include <glog/logging.h>
 #include <iosfwd>
 
-#define DREAL_LOG_FATAL   VLOG(0)
-#define DREAL_LOG_ERROR   VLOG(1)
-#define DREAL_LOG_WARNING VLOG(2)
-#define DREAL_LOG_INFO    VLOG(3)
+#define DREAL_FATAL_LEVEL   0
+#define DREAL_ERROR_LEVEL   1
+#define DREAL_WARNING_LEVEL 2
+#define DREAL_INFO_LEVEL    3
+
+#define DREAL_LOG_FATAL   VLOG(DREAL_FATAL_LEVEL)
+#define DREAL_LOG_ERROR   VLOG(DREAL_ERROR_LEVEL)
+#define DREAL_LOG_WARNING VLOG(DREAL_WARNING_LEVEL)
+#define DREAL_LOG_INFO    VLOG(DREAL_INFO_LEVEL)
+
+#define DREAL_LOG_FATAL_IS_ON   VLOG_IS_ON(DREAL_FATAL_LEVEL)
+#define DREAL_LOG_ERROR_IS_ON   VLOG_IS_ON(DREAL_ERROR_LEVEL)
+#define DREAL_LOG_WARNING_IS_ON VLOG_IS_ON(DREAL_WARNING_LEVEL)
+#define DREAL_LOG_INFO_IS_ON    VLOG_IS_ON(DREAL_INFO_LEVEL)
