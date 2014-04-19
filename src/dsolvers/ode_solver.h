c1d247cf9f5eca67512702de8a0c98034e7b29eb
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-19 06:07:38 -0400

    chore: update copyright info

--- src/dsolvers/ode_solver.h
+++ src/dsolvers/ode_solver.h
@@ -3,7 +3,7 @@ Author: Soonho Kong <soonhok@cs.cmu.edu>
         Sicun Gao <sicung@cs.cmu.edu>
         Edmund Clarke <emc@cs.cmu.edu>
 
-dReal -- Copyright (C) 2013, Soonho Kong, Sicun Gao, and Edmund Clarke
+dReal -- Copyright (C) 2013 - 2014, Soonho Kong, Sicun Gao, and Edmund Clarke
 
 dReal is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
