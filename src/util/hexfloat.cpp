c79a6e6ed774be15947830c742cb358dfbba2670
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-27 17:23:22 -0400

    feat(util/hexfloat): add hexfloat to print exact/hexdecimal rep. of double number
     - std::hexfloat should do the job. However current g++ does not support
       it yet[1].
     - Instead, we use "%a" format specifier[2] + snprintf.
    
    [1]: http://gcc.gnu.org/onlinedocs/libstdc++/manual/status.html
    [2]: http://www.cplusplus.com/reference/cstdio/printf/

---
+++ src/util/hexfloat.cpp
@@ -0,0 +1,39 @@
+/*********************************************************************
+Author: Soonho Kong <soonhok@cs.cmu.edu>
+        Sicun Gao <sicung@cs.cmu.edu>
+        Edmund Clarke <emc@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2014, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include <string>
+#include "util/string.h"
+#include "util/logging.h"
+
+using std::string;
+
+namespace dreal {
+string to_hexfloat(double x) {
+    static int const BUF_SIZE = 100;
+    char buf[BUF_SIZE];
+    int len = snprintf(buf, 100, "%a", x);
+    if (len >= BUF_SIZE) {
+        DREAL_LOG_WARNING << "to_hexfloat: printing " << x
+                          << " requires more than " << BUF_SIZE << " bytes of buffer.";
+    }
+    return buf;
+}
+}
