c9b73fa6fc4b8066578d74114e0dd90b2354ea30
Soonho Kong <soonhok@cs.cmu.edu>
2014-04-28 09:39:12 -0400

    fix(util/scoped_env): fix output format

--- src/util/scoped_env.cpp
+++ src/util/scoped_env.cpp
@@ -112,7 +112,7 @@ unsigned scoped_env::size() const {
 std::ostream & operator<<(std::ostream & out, scoped_env const & e) {
     for (auto const & p : e) {
         out << setfill(' ') << setw(15) << p.first
-            << " ==> " << p.second << endl;
+            << " : " << p.second << ";" << endl;
     }
     return out;
 }
