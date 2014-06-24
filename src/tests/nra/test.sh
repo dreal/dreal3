72467f4ca80643bdae1827e2ca9b5f922aadc491
Soonho Kong <soonhok@cs.cmu.edu>
2014-06-24 14:43:16 -0400

    tests(nra): add more testcases for --delta, --delta_heuristic, and --short_sat options

--- src/tests/nra/test.sh
+++ src/tests/nra/test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 DREAL=$1
 INSTANCE=$2
-EXPECTED_OUT=$2.expected
-TMP=$2.out
-OPTIONS=--delta --delta_heuristic --short_sat
-
-$DREAL $OPTIONS $INSTANCE | tee $TMP
+shift 2
+EXPECTED_OUT=${INSTANCE}.expected
+TMP=${INSTANCE}.out
+OPTIONS=$@
+echo $DREAL $OPTIONS -- "$INSTANCE"
+$DREAL $OPTIONS -- "$INSTANCE" | tee $TMP
 diff $TMP $EXPECTED_OUT
 RESULT=$?
 rm $TMP
