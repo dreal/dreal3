2ce687985389df82387d6210293db6747e09c977
Soonho Kong <soonhok@cs.cmu.edu>
2014-06-24 14:26:42 -0400

    fix(dsolvers/ode_solver): fix style (#include <algorithm>)

--- src/dsolvers/ode_solver.cpp
+++ src/dsolvers/ode_solver.cpp
@@ -19,6 +19,7 @@ You should have received a copy of the GNU General Public License
 along with dReal. If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
+#include <algorithm>
 #include <chrono>
 #include <iomanip>
 #include <limits>
