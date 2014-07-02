7b7a30a4a6a82a517c4e2d49733c2a6e5bb87a47
Soonho Kong <soonhok@cs.cmu.edu>
2014-07-02 03:00:42 -0400

    feat(util/scoped_vec): make scoped_vec template

--- src/util/scoped_vec.h
+++ src/util/scoped_vec.h
@@ -29,37 +29,63 @@ along with dReal. If not, see <http://www.gnu.org/licenses/>.
 // Backtrackable scoped_vec. -- Soonho Kong
 
 namespace dreal {
+template<typename T>
 class scoped_vec {
 private:
-    typedef std::vector<Enode *> vec;
-    typedef vec::value_type      value_type;
-    typedef vec::iterator        iterator;
-    typedef vec::const_iterator  const_iterator;
-    typedef vec::size_type       size_type;
-    typedef vec::reference       reference;
-    typedef vec::const_reference const_reference;
+    typedef std::vector<T> vec;
+    typedef typename vec::value_type      value_type;
+    typedef typename vec::iterator        iterator;
+    typedef typename vec::const_iterator  const_iterator;
+    typedef typename vec::size_type       size_type;
+    typedef typename vec::reference       reference;
+    typedef typename vec::const_reference const_reference;
 
     vec                   m_vec;
     std::vector<unsigned> m_scopes;
 
 public:
-    scoped_vec();
-    ~scoped_vec();
+    scoped_vec() { }
+    ~scoped_vec() { }
     iterator       begin()        { return m_vec.begin(); }
     iterator       end()          { return m_vec.end(); }
     const_iterator begin()  const { return m_vec.cbegin(); }
     const_iterator end()    const { return m_vec.cend(); }
     const_iterator cbegin() const { return m_vec.cbegin(); }
     const_iterator cend()   const { return m_vec.cend(); }
-    void push_back(value_type const & v);
-    void push();
-    void pop();
+    void push_back(value_type const & v) { m_vec.push_back(v); }
+    void push() { m_scopes.push_back(m_vec.size()); };
+    unsigned int pop() {
+        unsigned int count = 0;
+        unsigned const prev_size = m_scopes.back();
+        m_scopes.pop_back();
+        unsigned cur_size = m_vec.size();
+        while (cur_size-- > prev_size) { m_vec.pop_back(); count++; }
+        return count;
+    }
     unsigned size() const    { return m_vec.size(); }
     vec const & get_vec() { return m_vec; }
-    friend std::ostream & operator<<(std::ostream & out, scoped_vec const & e);
-    reference operator[] (size_type n);
-    const_reference operator[] (size_type n) const;
+    friend std::ostream & operator<<(std::ostream & out, scoped_vec<T> const & v) {
+        for (auto const & e : v) {
+            out << e << std::endl;
+        }
+        return out;
+    }
+    reference first() {
+        assert(m_vec.size() > 0);
+        return m_vec[0];
+    }
+    const_reference first() const {
+        assert(m_vec.size() > 0);
+        return m_vec[0]; }
+    reference last() {
+        assert(m_vec.size() > 0);
+        return m_vec[size() - 1];
+    }
+    const_reference last() const {
+        assert(m_vec.size() > 0);
+        return m_vec[size() - 1];
+    }
+    reference operator[] (size_type n) { return m_vec[n]; }
+    const_reference operator[] (size_type n) const { return m_vec[n]; }
 };
-
-std::ostream & operator<<(std::ostream & out, scoped_vec const & e);
 }
