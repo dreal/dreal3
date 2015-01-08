6e2cb6a4d51d128db096446f87cc08251184ad63
Soonho Kong <soonhok@cs.cmu.edu>
2015-01-09 00:59:23 +0900

    chore(opensmt/api/OpenSMTContext.C): fix indentation

--- src/opensmt/api/OpenSMTContext.C
+++ src/opensmt/api/OpenSMTContext.C
@@ -793,11 +793,11 @@ void OpenSMTContext::PrintResult( const lbool & result, const lbool & config_sta
 #endif
     if (config.produce_stats){
       for( auto t : getEgraphP()->getTSolvers()){
-	dreal::nra_solver* nra = dynamic_cast<dreal::nra_solver*>(t);
-	if(nra){
-	  //out << "nodes: " << solver.decisions << " " << nra->decisions() << endl;
-	  break;
-	}
+        dreal::nra_solver* nra = dynamic_cast<dreal::nra_solver*>(t);
+        if(nra){
+          //out << "nodes: " << solver.decisions << " " << nra->decisions() << endl;
+          break;
+        }
       }
     }
 
