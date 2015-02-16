4341d5a454e7f0dc1fe1bc78a200cf1075a2c5ea
Soonho Kong <soonhok@cs.cmu.edu>
2015-02-16 05:18:58 -0500

    fix(tests/nra/test.sh): use .option file if exists

--- src/tests/nra/test.sh
+++ src/tests/nra/test.sh
@@ -5,8 +5,13 @@ shift 2
 EXPECTED_OUT=${INSTANCE}.expected
 TMP=${INSTANCE}.out
 OPTIONS=$@
-echo $DREAL $OPTIONS -- "$INSTANCE"
-$DREAL $OPTIONS -- "$INSTANCE" | tee $TMP
+OPTION=
+if [[ -e $2.option ]]
+then
+    OPTION=`cat $2.option`
+fi
+echo $DREAL $OPTIONS $OPTION -- "$INSTANCE"
+$DREAL $OPTIONS $OPTION -- "$INSTANCE" | tee $TMP
 diff $TMP $EXPECTED_OUT
 RESULT=$?
 rm $TMP
