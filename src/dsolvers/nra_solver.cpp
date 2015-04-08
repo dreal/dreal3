608552781aca44cbdf33f0e3e21c533c8be91390
Soonho Kong <soonhok@cs.cmu.edu>
2015-04-08 05:55:44 -0400

    feat(dsolvers/nra_solver): handle --visualize option

--- src/dsolvers/nra_solver.cpp
+++ src/dsolvers/nra_solver.cpp
@@ -373,16 +373,19 @@ void nra_solver::handle_sat_case(box const & b) const {
 #ifdef SUPPORT_ODE
     // --visualize option
     if (config.nra_json) {
+        json traces = {};
         // Need to run ODE pruning operator once again to generate a trace
         for (constraint * const ctr : m_stack) {
             if (ctr->get_type() == constraint_type::ODE) {
                 contractor_capd_fwd_full fwd_full(b, dynamic_cast<ode_constraint *>(ctr), config.nra_ODE_taylor_order, config.nra_ODE_grid_size);
                 json trace = fwd_full.generate_trace(b, config);
-                config.nra_json_out << trace.dump() << endl;;
+                traces.push_back(trace);
             }
         }
+        json vis_json;
+        vis_json["traces"] = traces;
+        config.nra_json_out << vis_json.dump() << endl;;
     }
-
 #endif
     // For API call
     b.assign_to_enode();
@@ -420,12 +423,12 @@ bool nra_solver::check(bool complete) {
 }
 
 vector<Enode *> nra_solver::generate_explanation(scoped_vec<constraint const *> const & ctr_vec) {
-    vector<Enode *> exps;
     unordered_set<Enode *> bag;
     for (constraint const * ctr : ctr_vec) {
         vector<Enode *> const & enodes_in_ctr = ctr->get_enodes();
         bag.insert(enodes_in_ctr.begin(), enodes_in_ctr.end());
     }
+    vector<Enode *> exps;
     copy(bag.begin(), bag.end(), back_inserter(exps));
     return exps;
 }
