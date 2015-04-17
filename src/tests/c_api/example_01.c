b8334e2149bc3ad9431ed498386427edaf65db87
Soonho Kong <soonhok@cs.cmu.edu>
2015-04-17 14:48:38 +0100

    test(c_api): update examples

--- src/tests/c_api/example_01.c
+++ src/tests/c_api/example_01.c
@@ -1,126 +1,152 @@
 #include "opensmt/api/opensmt_c.h"
 #include <stdio.h>
 
+void print_sol(opensmt_context ctx, opensmt_expr x, opensmt_expr y, opensmt_expr z) {
+    double const x_lb = opensmt_get_lb(ctx, x);
+    double const x_ub = opensmt_get_ub(ctx, x);
+    fprintf(stderr, "x = [%f, %f]\n", x_lb, x_ub);
+    double const y_lb = opensmt_get_lb(ctx, y);
+    double const y_ub = opensmt_get_ub(ctx, y);
+    fprintf(stderr, "y = [%f, %f]\n", y_lb, y_ub);
+    double const z_lb = opensmt_get_lb(ctx, z);
+    double const z_ub = opensmt_get_ub(ctx, z);
+    fprintf(stderr, "z = [%f, %f]\n", z_lb, z_ub);
+    return;
+}
+
 int main( int argc, char * argv[] ) {
     // Creating context
-    printf( "Creating context\n" );
+    fprintf(stderr, "Creating context\n" );
+    opensmt_init();
     opensmt_context ctx = opensmt_mk_context( qf_nra );
 
     // Setting verbosity
     opensmt_set_verbosity( ctx, 4 );
 
     // Creating boolean variables
-    printf( "Creating some boolean variables\n" );
+    fprintf(stderr, "Creating some boolean variables\n" );
     opensmt_expr a = opensmt_mk_bool_var( ctx, "a" );
     opensmt_expr b = opensmt_mk_bool_var( ctx, "b" );
     opensmt_expr c = opensmt_mk_bool_var( ctx, "c" );
     // Creating integer variables
-    printf( "Creating some integer variables\n" );
-    opensmt_expr x = opensmt_mk_int_var( ctx, "x" , -100, 100);
-    opensmt_expr y = opensmt_mk_int_var( ctx, "y" , -100, 100);
-    opensmt_expr z = opensmt_mk_int_var( ctx, "z" , -100, 100);
+    fprintf(stderr, "Creating some integer variables\n" );
+    opensmt_expr x = opensmt_mk_int_var( ctx, "x" , -10, 10);
+    opensmt_expr y = opensmt_mk_int_var( ctx, "y" , -10, 10);
+    opensmt_expr z = opensmt_mk_int_var( ctx, "z" , -10, 10);
     // Creating inequality
-    printf( "Creating x - y <= 0\n" );
+    fprintf(stderr, "Creating x - y <= 0\n" );
 
-    printf( "  Creating x - y\n" );
+    fprintf(stderr, "  Creating x - y\n" );
     opensmt_expr minus = opensmt_mk_minus( ctx, x, y );
-    printf( "  Expression created: " );
+    fprintf(stderr, "  Expression created: " );
     opensmt_print_expr( minus );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
 
-    printf( "  Creating 0\n" );
+    fprintf(stderr, "  Creating 0\n" );
     opensmt_expr zero = opensmt_mk_num_from_string( ctx, "0" );
-    printf( "  Expression created: " );
+    fprintf(stderr, "  Expression created: " );
     opensmt_print_expr( zero );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
 
-    printf( "  Creating x - y <= 0\n" );
+    fprintf(stderr, "  Creating x - y <= 0\n" );
     opensmt_expr leq = opensmt_mk_leq( ctx, minus, zero );
-    printf( "  Expression created: " );
+    fprintf(stderr, "  Expression created: " );
     opensmt_print_expr( leq );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
 
     // Creating inequality 2
-    printf( "Creating y - z <= 0\n" );
+    fprintf(stderr, "Creating y - z <= 0\n" );
     opensmt_expr minus2 = opensmt_mk_minus( ctx, y, z );
     opensmt_expr leq2 = opensmt_mk_leq( ctx, minus2, zero );
-    printf( "  Expression created: " );
+    fprintf(stderr, "  Expression created: " );
     opensmt_print_expr( leq2 );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
 
     // Creating inequality 3
-    printf( "Creating z - x <= -1\n" );
+    fprintf(stderr, "Creating z - x <= -1\n" );
     opensmt_expr minus3 = opensmt_mk_minus( ctx, z, x );
     opensmt_expr mone = opensmt_mk_num_from_string( ctx, "-1" );
     opensmt_expr leq3 = opensmt_mk_leq( ctx, minus3, mone );
-    printf( "  Expression created: " );
+    fprintf(stderr, "  Expression created: " );
     opensmt_print_expr( leq3 );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
 
     // Creating inequality 4
-    printf( "Creating z - x <= 0\n" );
+    fprintf(stderr, "Creating z - x <= 0\n" );
     opensmt_expr minus4 = opensmt_mk_minus( ctx, z, x );
     opensmt_expr leq4 = opensmt_mk_leq( ctx, minus4, zero );
-    printf( "  Expression created: " );
+    fprintf(stderr, "  Expression created: " );
     opensmt_print_expr( leq4 );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
 
     // Asserting first inequality
-    printf( "Asserting ");
+    fprintf(stderr, "Asserting ");
     opensmt_print_expr( leq );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
     opensmt_assert( ctx, leq );
     opensmt_push( ctx );
 
     // Checking for consistency
-    printf( "\nChecking for consistency: " );
+    fprintf(stderr, "\nChecking for consistency: " );
     opensmt_result res = opensmt_check( ctx );
-    printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+    fprintf(stderr, "%s\n\n", res == l_false ? "unsat" : "sat" );
+    if (res == l_true) {
+        print_sol(ctx, x, y, z);
+    }
 
     // Asserting second inequality
-    printf( "Asserting ");
+    fprintf(stderr, "Asserting ");
     opensmt_print_expr( leq2 );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
     opensmt_assert( ctx, leq2 );
     opensmt_push( ctx );
 
     // Checking for consistency
-    printf( "\nChecking for consistency: " );
+    fprintf(stderr, "\nChecking for consistency: " );
     res = opensmt_check( ctx );
-    printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+    fprintf(stderr, "%s\n\n", res == l_false ? "unsat" : "sat" );
+    if (res == l_true) {
+        print_sol(ctx, x, y, z);
+    }
 
     // Asserting third inequality
-    printf( "Asserting ");
+    fprintf(stderr, "Asserting ");
     opensmt_print_expr( leq3 );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
     opensmt_assert( ctx, leq3 );
     opensmt_push( ctx );
 
     // Checking for consistency - it is UNSAT
-    printf( "\nChecking for consistency: " );
+    fprintf(stderr, "\nChecking for consistency: " );
     res = opensmt_check( ctx );
-    printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+    fprintf(stderr, "%s\n\n", res == l_false ? "unsat" : "sat" );
+    if (res == l_true) {
+        print_sol(ctx, x, y, z);
+    }
 
     // Backtracking one step - so the state is SAT again
     opensmt_pop( ctx );
-    printf( "Backtracking one step\n\n" );
+    fprintf(stderr, "Backtracking one step\n\n" );
 
     // Asserting fourth inequality
-    printf( "Asserting ");
+    fprintf(stderr, "Asserting ");
     opensmt_print_expr( leq4 );
-    printf( "\n" );
+    fprintf(stderr, "\n" );
     opensmt_assert( ctx, leq4 );
 
     // Checking for consistency
-    printf( "\nChecking for consistency: " );
+    fprintf(stderr, "\nChecking for consistency: " );
     res = opensmt_check( ctx );
-    printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+    fprintf(stderr, "%s\n\n", res == l_false ? "unsat" : "sat" );
+    if (res == l_true) {
+        print_sol(ctx, x, y, z);
+    }
 
     // Resetting context
-    printf( "Resetting context\n" );
+    fprintf(stderr, "Resetting context\n" );
     opensmt_reset( ctx );
     // Deleting context
-    printf( "Deleting context\n" );
+    fprintf(stderr, "Deleting context\n" );
     opensmt_del_context( ctx );
 
     return 0;
--- src/tests/c_api/example_02.c
+++ src/tests/c_api/example_02.c
@@ -4,20 +4,34 @@
 int main( int argc, char * argv[] ) {
     opensmt_init();
     opensmt_context ctx = opensmt_mk_context( qf_nra );
-    opensmt_set_precision (ctx, 0.00001);
+    opensmt_set_precision (ctx, 0.001);
     opensmt_expr x = opensmt_mk_real_var( ctx, "x" , -3.141592, 3.141592);
     opensmt_expr y = opensmt_mk_real_var( ctx, "y" , -3.141592, 3.141592);
-    opensmt_expr eq = opensmt_mk_eq( ctx, opensmt_mk_sin(ctx, x), opensmt_mk_cos(ctx, y));
-    opensmt_assert( ctx, eq );
+    opensmt_expr eq1 = opensmt_mk_eq( ctx, opensmt_mk_sin(ctx, x), opensmt_mk_cos(ctx, y));
+    opensmt_assert( ctx, eq1 );
     opensmt_result res = opensmt_check( ctx );
-    printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+    fprintf(stderr, "%s\n\n", res == l_false ? "unsat" : "sat" );
     if (res == l_true) {
         double const x_lb = opensmt_get_lb(ctx, x);
         double const x_ub = opensmt_get_ub(ctx, x);
-        printf("x = [%f, %f]\n", x_lb, x_ub);
+        fprintf(stderr, "x = [%f, %f]\n", x_lb, x_ub);
         double const y_lb = opensmt_get_lb(ctx, y);
         double const y_ub = opensmt_get_ub(ctx, y);
-        printf("y = [%f, %f]\n", y_lb, y_ub);
+        fprintf(stderr, "y = [%f, %f]\n", y_lb, y_ub);
     }
+
+    opensmt_expr eq2 = opensmt_mk_eq( ctx, opensmt_mk_tan(ctx, x), opensmt_mk_sin(ctx, y));
+    opensmt_assert( ctx, eq2 );
+    res = opensmt_check( ctx );
+    fprintf(stderr, "%s\n\n", res == l_false ? "unsat" : "sat" );
+    if (res == l_true) {
+        double const x_lb = opensmt_get_lb(ctx, x);
+        double const x_ub = opensmt_get_ub(ctx, x);
+        fprintf(stderr, "x = [%f, %f]\n", x_lb, x_ub);
+        double const y_lb = opensmt_get_lb(ctx, y);
+        double const y_ub = opensmt_get_ub(ctx, y);
+        fprintf(stderr, "y = [%f, %f]\n", y_lb, y_ub);
+    }
+    opensmt_del_context( ctx );
     return 0;
 }
