9d0a019bbeaff9994a7eff08702e77ee09a1b6b7
Soonho Kong <soonhok@cs.cmu.edu>
2015-05-08 07:05:52 -0400

    test(ibex): fix style of example_01

--- src/tests/ibex/example_01.cpp
+++ src/tests/ibex/example_01.cpp
@@ -1,7 +1,31 @@
+/*********************************************************************
+Author: Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2015, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
 #include <iostream>
 #include "ibex/ibex.h"
 
-using namespace ibex;
+using ibex::Variable;
+using ibex::Function;
+using ibex::NumConstraint;
+using ibex::CtcFwdBwd;
+using ibex::IntervalVector;
+using ibex::Interval;
 using std::cout;
 using std::endl;
 
@@ -13,9 +37,9 @@ int main() {
     CtcFwdBwd ctc(c);
 
     IntervalVector box(3);
-    box[0] = Interval(1,1);
-    box[1] = Interval(0,0);
-    box[2] = Interval(-100,100);
+    box[0] = Interval(1, 1);
+    box[1] = Interval(0, 0);
+    box[2] = Interval(-100, 100);
 
     cout << "Before contract: " << box << endl;
     ctc.contract(box);
