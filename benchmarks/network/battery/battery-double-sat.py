a9bc823cf1cc48b3bbae29ab767ca3d95328d0bc
Kyungmin Bae <kquine@gmail.com>
2015-05-11 10:21:24 -0400

    benchmark(network): pintegral/connect benchmarks added

---
+++ benchmarks/network/battery/battery-double-sat.py
@@ -0,0 +1,126 @@
+
+from gen import *
+
+##########
+# shared #
+##########
+
+flow_var[0] = """
+(declare-fun tau () Real)
+(declare-fun d1 () Real)
+(declare-fun g1 () Real)
+(declare-fun d2 () Real)
+(declare-fun g2 () Real)
+"""
+
+flow_dec[0] = """
+(define-ode flow_1 ((= d/dt[d1] (- (/ 0.5 0.166) (* 0.122 d1))) (= d/dt[g1] -0.5) (= d/dt[d2] (- (/ 0.5 0.166) (* 0.122 d2))) (= d/dt[g2] -0.5) (= d/dt[tau] 1)))
+(define-ode flow_2 ((= d/dt[d1] (- (/ 1 0.166) (* 0.122 d1))) (= d/dt[g1] -1) (= d/dt[d2] (- 0 (* 0.122 d2))) (= d/dt[g2] 0) (= d/dt[tau] 1)))
+(define-ode flow_3 ((= d/dt[d1] (- 0 (* 0.122 d1))) (= d/dt[g1] 0) (= d/dt[d2] (- (/ 1 0.166) (* 0.122 d2))) (= d/dt[g2] -1) (= d/dt[tau] 1)))
+(define-ode flow_4 ((= d/dt[d1] 0) (= d/dt[g1] 0) (= d/dt[d2] (- (/ 1 0.166) (* 0.122 d2))) (= d/dt[g2] -1) (= d/dt[tau] 1)))
+(define-ode flow_5 ((= d/dt[d1] (- (/ 1 0.166) (* 0.122 d1))) (= d/dt[g1] -1) (= d/dt[d2] 0) (= d/dt[g2] 0) (= d/dt[tau] 1)))
+(define-ode flow_6 ((= d/dt[d1] 0) (= d/dt[g1] 0) (= d/dt[d2] 0) (= d/dt[g2] 0) (= d/dt[tau] 1)))
+"""
+
+state_dec[0] = """
+(declare-fun time_{0} () Real)  
+(declare-fun tau_{0}_0 () Real) 
+(declare-fun tau_{0}_t () Real) 
+(declare-fun mode1U_{0} () Bool) 
+(declare-fun mode1S_{0} () Bool) 
+(declare-fun d1_{0}_0 () Real)  
+(declare-fun d1_{0}_t () Real)  
+(declare-fun g1_{0}_0 () Real)  
+(declare-fun g1_{0}_t () Real)  
+(declare-fun mode2U_{0} () Bool) 
+(declare-fun mode2S_{0} () Bool) 
+(declare-fun d2_{0}_0 () Real)  
+(declare-fun d2_{0}_t () Real)  
+(declare-fun g2_{0}_0 () Real)  
+(declare-fun g2_{0}_t () Real)  
+"""
+
+state_val[0] = """
+(assert (<= 0 time_{0}))  (assert (<= time_{0} 0.01))
+(assert (<= 0 tau_{0}_0)) (assert (<= tau_{0}_0 50))
+(assert (<= 0 tau_{0}_t)) (assert (<= tau_{0}_t 50))
+(assert (<= -10 d1_{0}_0)) (assert (<= d1_{0}_0 10))
+(assert (<= -10 d1_{0}_t)) (assert (<= d1_{0}_t 10))
+(assert (<= -10 g1_{0}_0)) (assert (<= g1_{0}_0 10))
+(assert (<= -10 g1_{0}_t)) (assert (<= g1_{0}_t 10))
+(assert (<= -10 d2_{0}_0)) (assert (<= d2_{0}_0 10))
+(assert (<= -10 d2_{0}_t)) (assert (<= d2_{0}_t 10))
+(assert (<= -10 g2_{0}_0)) (assert (<= g2_{0}_0 10))
+(assert (<= -10 g2_{0}_t)) (assert (<= g2_{0}_t 10))
+(assert (or 
+  (and (= mode1U_{0} true) (= mode1S_{0} true) (= mode2U_{0} true) (= mode2S_{0} true))
+  (and (= mode1U_{0} true) (= mode1S_{0} false) (= mode2U_{0} false) (= mode2S_{0} true))
+  (and (= mode1U_{0} true) (= mode1S_{0} false) (= mode2U_{0} false) (= mode2S_{0} false))
+  (and (= mode1U_{0} false) (= mode1S_{0} true) (= mode2U_{0} true) (= mode2S_{0} false))
+  (and (= mode1U_{0} false) (= mode1S_{0} false) (= mode2U_{0} true) (= mode2S_{0} false))
+  (and (= mode1U_{0} false) (= mode1S_{0} false) (= mode2U_{0} false) (= mode2S_{0} false))))
+"""
+
+cont_cond[0] = ["""
+(assert (or
+  (and (= mode1U_{0} true) (= mode1S_{0} true) (= mode2U_{0} true) (= mode2S_{0} true)
+       (= [d1_{0}_t g1_{0}_t g2_{0}_t d2_{0}_t tau_{0}_t] 
+          (integral 0. time_{0} [d1_{0}_0 g1_{0}_0 g2_{0}_0 d2_{0}_0 tau_{0}_0] flow_1)))
+  (and (= mode1U_{0} true) (= mode1S_{0} false) (= mode2U_{0} false) (= mode2S_{0} true)
+       (= [d1_{0}_t g1_{0}_t g2_{0}_t d2_{0}_t tau_{0}_t] 
+          (integral 0. time_{0} [d1_{0}_0 g1_{0}_0 g2_{0}_0 d2_{0}_0 tau_{0}_0] flow_2)))
+  (and (= mode1U_{0} false) (= mode1S_{0} true) (= mode2U_{0} true) (= mode2S_{0} false)
+       (= [d1_{0}_t g1_{0}_t g2_{0}_t d2_{0}_t tau_{0}_t] 
+          (integral 0. time_{0} [d1_{0}_0 g1_{0}_0 g2_{0}_0 d2_{0}_0 tau_{0}_0] flow_3)))
+  (and (= mode1U_{0} false) (= mode1S_{0} false) (= mode2U_{0} true) (= mode2S_{0} false)
+       (= [d1_{0}_t g1_{0}_t g2_{0}_t d2_{0}_t tau_{0}_t] 
+          (integral 0. time_{0} [d1_{0}_0 g1_{0}_0 g2_{0}_0 d2_{0}_0 tau_{0}_0] flow_4)))
+  (and (= mode1U_{0} true) (= mode1S_{0} false) (= mode2U_{0} false) (= mode2S_{0} false)
+       (= [d1_{0}_t g1_{0}_t g2_{0}_t d2_{0}_t tau_{0}_t] 
+          (integral 0. time_{0} [d1_{0}_0 g1_{0}_0 g2_{0}_0 d2_{0}_0 tau_{0}_0] flow_5)))
+  (and (= mode1U_{0} false) (= mode1S_{0} false) (= mode2U_{0} false) (= mode2S_{0} false)
+       (= [d1_{0}_t g1_{0}_t g2_{0}_t d2_{0}_t tau_{0}_t] 
+          (integral 0. time_{0} [d1_{0}_0 g1_{0}_0 g2_{0}_0 d2_{0}_0 tau_{0}_0] flow_6)))))
+"""]
+
+jump_cond[0] = ["""
+(assert (= tau_{1}_0 tau_{0}_t))
+(assert (and (= d1_{1}_0 d1_{0}_t) (= g1_{1}_0 g1_{0}_t)))
+(assert (or (and (<= g1_0_t (* (- 1 0.166) d1_0_t)) 
+                 (= mode1U_{1} false) (= mode1S_{1} false))
+            (and (>  g1_0_t (* (- 1 0.166) d1_0_t)) 
+                 (not (and (= mode1U_{0} false) (= mode1S_{0} false)))
+	         (not (and (= mode1U_{1} false) (= mode1S_{1} false))))))
+(assert (and (= d2_{1}_0 d2_{0}_t) (= g2_{1}_0 g2_{0}_t)))
+(assert (or (and (<= g2_0_t (* (- 1 0.166) d2_0_t)) 
+                 (= mode2U_{1} false) (= mode2S_{1} false))
+            (and (>  g2_0_t (* (- 1 0.166) d2_0_t)) 
+                 (not (and (= mode2U_{0} false) (= mode2S_{0} false)))
+	         (not (and (= mode2U_{1} false) (= mode2S_{1} false))))))"""]
+
+#############
+# Init/Goal #
+#############
+
+init_cond = """
+(assert (= tau_{0}_0 0))
+(assert (and (= mode1U_{0} true) (= mode1S_{0} true)))
+(assert (and (= g1_{0}_0 8.5) (= d1_{0}_0 0)))
+(assert (and (= mode2U_{0} true) (= mode2S_{0} true)))
+(assert (and (= g2_{0}_0 7.5) (= d2_{0}_0 0)))
+"""
+
+goal_cond = """
+(assert (and (>= tau_{0}_t 0.01)
+             (not (and (= mode1U_{0} false) (= mode1S_{0} false) 
+                       (= mode2U_{0} false) (= mode2S_{0} false)))))
+"""
+
+import sys
+try:
+    bound = int(sys.argv[1])
+except:
+    print("Usage:", sys.argv[0], "<Bound>")
+else:
+    generate(bound, 1, [0], 3, init_cond, goal_cond)
+
---
+++ benchmarks/network/water/water-double-p-sat.py
@@ -0,0 +1,133 @@
+
+from gen import *
+
+##########
+# shared #
+##########
+
+flow_var[0] = """
+(declare-fun tau () Real)
+"""
+
+flow_dec[0] = """
+(define-ode flow_1 ((= d/dt[tau] 1)))
+"""
+
+state_dec[0] = """
+(declare-fun time_{0} () Real)  
+(declare-fun tau_{0}_0 () Real) 
+(declare-fun tau_{0}_t () Real) 
+"""
+
+state_val[0] = """
+(assert (<= 0 time_{0}))  (assert (<= time_{0} 1))
+(assert (<= 0 tau_{0}_0)) (assert (<= tau_{0}_0 1))
+(assert (<= 0 tau_{0}_t)) (assert (<= tau_{0}_t 1))
+"""
+
+cont_cond[0] = ["""
+(assert (and (>= tau_{0}_0 0) (<= tau_{0}_0 1)
+             (>= tau_{0}_t 0) (<= tau_{0}_t 1)))
+(assert (and (= [x1_{0}_t x2_{0}_t tau_{0}_t] 
+                (pintegral 0. time_{0} 
+                           [x1_{0}_0 x2_{0}_0 tau_{0}_0] 
+                           [holder_{1} holder_{2} holder_{3}]))
+             (connect holder_{3} flow_1)))"""]
+
+jump_cond[0] = ["""
+(assert (and (= tau_{0}_t 1) (= tau_{1}_0 0)))"""]
+
+################
+# water tank 1 #
+################
+
+flow_var[1] = """
+(declare-fun x1 () Real)
+"""
+
+flow_dec[1] = """
+(define-ode flow_2 ((= d/dt[x1] (/ (- 5 (* (* 0.5 (^ (* 2 9.80665) 0.5)) (^ x1 0.5))) 2))))
+(define-ode flow_3 ((= d/dt[x1] (/ (* (* -0.5 (^ (* 2 9.80665) 0.5)) (^ x1 0.5)) 2))))
+"""
+
+state_dec[1] = """
+(declare-fun mode1_{0} () Bool)
+(declare-fun x1_{0}_0 () Real)
+(declare-fun x1_{0}_t () Real)
+"""
+
+state_val[1] = """
+(assert (<= 0 x1_{0}_0)) (assert (<= x1_{0}_0 10))
+(assert (<= 0 x1_{0}_t)) (assert (<= x1_{0}_t 10))
+"""
+
+cont_cond[1] = ["""
+(assert (or (and (= mode1_{0} true) (connect holder_{1} flow_2))
+            (and (= mode1_{0} false) (connect holder_{1} flow_3))))
+(assert (not (and (connect holder_{1} flow_2) (connect holder_{1} flow_3))))"""]
+
+jump_cond[1] = ["""
+(assert (and (= x1_{1}_0 x1_{0}_t)))
+(assert (or (and (< x1_{0}_t 5) (= mode1_{1} true))
+            (and (>= x1_{0}_t 5) (= mode1_{1} false))))"""]
+
+################
+# water tank 2 #
+################
+
+flow_var[2] = """
+(declare-fun x2 () Real)
+"""
+
+flow_dec[2] = """
+(define-ode flow_4 ((= d/dt[x2] (/ (+ 3 (* (* 0.5 (^ (* 2 9.80665) 0.5)) (- (^ x1 0.5) (^ x2 0.5)))) 4))))
+(define-ode flow_5 ((= d/dt[x2] (/ (* (* 0.5 (^ (* 2 9.80665) 0.5)) (- (^ x1 0.5) (^ x2 0.5))) 4))))
+"""
+
+state_dec[2] = """
+(declare-fun mode2_{0} () Bool)
+(declare-fun x2_{0}_0 () Real)  
+(declare-fun x2_{0}_t () Real)  
+"""
+
+state_val[2] = """
+(assert (<= 0 x2_{0}_0)) (assert (<= x2_{0}_0 10))
+(assert (<= 0 x2_{0}_t)) (assert (<= x2_{0}_t 10))
+"""
+
+cont_cond[2] = ["""
+(assert (or (and (= mode2_{0} true)  (connect holder_{2} flow_4))
+            (and (= mode2_{0} false) (connect holder_{2} flow_5))))
+(assert (not (and (connect holder_{2} flow_4) (connect holder_{2} flow_5))))"""]
+
+jump_cond[2] = ["""
+(assert (and (= x2_{1}_0 x2_{0}_t)))
+(assert (or (and (< x2_{0}_t 5) (= mode2_{1} true))
+            (and (>= x2_{0}_t 5) (= mode2_{1} false))))"""]
+
+
+#############
+# Init/Goal #
+#############
+
+init_cond = """
+(assert (= tau_{0}_0 0))
+(assert (= mode1_{0} true))
+(assert (and (>= x1_{0}_0 (- 5 0.1)) (<= x1_{0}_0 (+ 5 0.1))))
+(assert (= mode2_{0} true))
+(assert (and (>= x2_{0}_0 (- 5 0.1)) (<= x2_{0}_0 (+ 5 0.1))))
+"""
+
+goal_cond = """
+(assert (or (< x1_{0}_t (- 5 0.1)) (> x1_{0}_t (+ 5 0.1))))
+(assert (or (< x2_{0}_t (- 5 0.1)) (> x2_{0}_t (+ 5 0.1))))
+"""
+
+import sys
+try:
+    bound = int(sys.argv[1])
+except:
+    print("Usage:", sys.argv[0], "<Bound>")
+else:
+    generate(bound, 1, [0,1,2], 3, init_cond, goal_cond)
+
