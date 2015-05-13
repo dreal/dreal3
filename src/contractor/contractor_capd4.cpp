9f96673abab0d61bac2e5210a3b6f1f81039c7a7
Soonho Kong <soonhok@cs.cmu.edu>
2015-05-13 07:04:10 -0400

    fix(contractor/contractor_capd4): check empty box after intersect_params
    close: #114

--- src/contractor/contractor_capd4.cpp
+++ src/contractor/contractor_capd4.cpp
@@ -472,6 +472,9 @@ box contractor_capd_fwd_full::prune(box b, SMTConfig &) const {
     DREAL_LOG_INFO << "contractor_capd_fwd_full::prune";
     integral_constraint const & ic = m_ctr->get_ic();
     b = intersect_params(b, ic);
+    if (b.is_empty()) {
+        return b;
+    }
     if (!m_solver) {
         // Trivial Case where there are only params and no real ODE vars.
         return b;
@@ -660,6 +663,9 @@ box contractor_capd_bwd_full::prune(box b, SMTConfig &) const {
     DREAL_LOG_INFO << "contractor_capd_bwd_full::prune";
     integral_constraint const & ic = m_ctr->get_ic();
     b = intersect_params(b, ic);
+    if (b.is_empty()) {
+        return b;
+    }
     if (!m_solver) {
         // Trivial Case where there are only params and no real ODE vars.
         return b;
