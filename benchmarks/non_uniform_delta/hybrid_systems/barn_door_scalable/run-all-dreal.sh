5ffd8c24efe2ef8fe41bb860545f5299a316bd45
Soonho Kong <soonhok@cs.cmu.edu>
2015-05-14 16:20:04 -0400

    benchmark: use "--precision N" instead of "--precision=N"

--- benchmarks/non_uniform_delta/hybrid_systems/barn_door_scalable/run-all-dreal.sh
+++ benchmarks/non_uniform_delta/hybrid_systems/barn_door_scalable/run-all-dreal.sh
@@ -3,17 +3,17 @@
 NUM=2
 
 for((i=2; i <=$NUM; i++)); do {
-	if [ ${i} -lt 10 ]; then
-	    PREFIX="0"
-	 else
-	    PREFIX=""
-	 fi
-	for d in 0.001 0.1; do {
-	for j in "" "_delta_all"  "_delta_timeonly" "_delta_aim" ; do {
-		INSTANCE=barn_door_${PREFIX}${i}_${i}${j}.smt2
-		#echo delta=${d}, steps = ${i}, variation = ${j}
-		echo $INSTANCE $d
-		time ../../../../dReal --delta --precision=${d} --proof $INSTANCE
-	}; done
+        if [ ${i} -lt 10 ]; then
+            PREFIX="0"
+         else
+            PREFIX=""
+         fi
+        for d in 0.001 0.1; do {
+        for j in "" "_delta_all"  "_delta_timeonly" "_delta_aim" ; do {
+                INSTANCE=barn_door_${PREFIX}${i}_${i}${j}.smt2
+                #echo delta=${d}, steps = ${i}, variation = ${j}
+                echo $INSTANCE $d
+                time ../../../../dReal --delta --precision ${d} --proof $INSTANCE
+        }; done
     }; done
 }; done
