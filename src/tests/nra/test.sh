476ce5fd833f52c035561b13c47af63f3f3f3f33
Soonho Kong <soonhok@cs.cmu.edu>
2015-05-28 15:46:22 -0400

    test(nra,nra_ode): fix test.sh to use temporary output file

--- src/tests/nra/test.sh
+++ src/tests/nra/test.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 DREAL=$1
 INSTANCE=$2
+BASENAME=`basename ${INSTANCE}`
 shift 2
 EXPECTED_OUT=${INSTANCE}.expected
-TMP=${INSTANCE}.out
+TMP=`mktemp /tmp/${BASENAME}.out.XXXX`
 OPTIONS=$@
 OPTION=
 if [[ -e ${INSTANCE}.option ]]
 then
-    OPTION=`cat ${INSTANCE}.option`
+    OPTION=`cat "${INSTANCE}.option"`
 fi
-echo $DREAL $OPTIONS $OPTION "$INSTANCE"
-$DREAL $OPTIONS $OPTION "$INSTANCE" | tee $TMP
-diff $TMP $EXPECTED_OUT
+echo ${DREAL} ${OPTIONS} ${OPTION} "${INSTANCE}" "==>" "${TMP}"
+"${DREAL}" ${OPTIONS} ${OPTION} "${INSTANCE}" | tee "${TMP}"
+diff "${TMP}" "${EXPECTED_OUT}"
 RESULT=$?
-rm $TMP
-exit $RESULT
+rm -- "${TMP}"
+exit ${RESULT}
--- src/tests/nra_ode/test.sh
+++ src/tests/nra_ode/test.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 DREAL=$1
 INSTANCE=$2
+BASENAME=`basename ${INSTANCE}`
 EXPECTED_OUT=$2.expected
 OPTION=
 if [[ -e $2.option ]]
 then
     OPTION=`cat $2.option`
 fi
-TMP=$2.out
+TMP=`mktemp /tmp/${BASENAME}.out.XXXX`
 
-echo $DREAL $OPTION $INSTANCE
-$DREAL $OPTION $INSTANCE | tee $TMP
-diff $TMP $EXPECTED_OUT
+echo "${DREAL} ${OPTION} ${INSTANCE} ==> ${TMP}"
+"${DREAL}" ${OPTION} "${INSTANCE}" | tee "${TMP}"
+diff "$TMP" "$EXPECTED_OUT"
 RESULT=$?
-rm $TMP
-exit $RESULT
+rm -- "${TMP}"
+exit ${RESULT}
