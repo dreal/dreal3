96abdf2427257a46a3931433bd3a31f8c5aeca48
Soonho Kong <soonhok@cs.cmu.edu>
2015-06-01 12:21:20 -0400

    test(c_api): add leak_test

---
+++ src/tests/c_api/leak_test.c
@@ -0,0 +1,46 @@
+/*********************************************************************
+Author: Soonho Kong <soonhok@cs.cmu.edu>
+        Sicun Gao <sicung@cs.cmu.edu>
+        Edmund Clarke <emc@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2015, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include <stdio.h>
+#include "opensmt/api/opensmt_c.h"
+
+// Test Memory Leaks
+int main(int argc, char * argv[]) {
+    int i = 0;
+    opensmt_init();
+    opensmt_context ctx = opensmt_mk_context(qf_nra);
+    opensmt_set_precision (ctx, 0.0000001);
+    opensmt_expr x = opensmt_mk_real_var(ctx, "x" , 0.0, 1.0);
+    for (i=0;i<100;++i) {
+        opensmt_push(ctx);
+        opensmt_expr lb = opensmt_mk_num_from_string(ctx, "0.1");
+        opensmt_expr ub = opensmt_mk_num_from_string(ctx, "0.9");
+        opensmt_expr geq = opensmt_mk_geq(ctx, x, lb);
+        opensmt_expr leq = opensmt_mk_leq(ctx, x, lb);
+        opensmt_assert(ctx, geq);
+        opensmt_assert(ctx, leq);
+        opensmt_result res = opensmt_check( ctx );
+        printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+        opensmt_pop(ctx);
+    }
+    opensmt_del_context(ctx);
+    return 0;
+}
