ed461937ef786eaf6e0f6220779e51938e3eb5ec
Soonho Kong <soonhok@cs.cmu.edu>
2015-06-08 12:39:35 -0400

    test(contractor/contractor_ibex): add more assertions

--- src/tests/contractor/contractor_ibex.cpp
+++ src/tests/contractor/contractor_ibex.cpp
@@ -31,8 +31,9 @@ using std::cerr;
 using std::endl;
 
 namespace dreal {
+
 TEST_CASE("ibex_fwdbwd_01") {
-    opensmt_init();
+    cerr << "===================================================\n";
     opensmt_context ctx = opensmt_mk_context(qf_nra);
     OpenSMTContext * opensmt_ctx = static_cast<OpenSMTContext *>(ctx);
     opensmt_set_precision(ctx, 0.0000001);
@@ -54,21 +55,29 @@ TEST_CASE("ibex_fwdbwd_01") {
     contractor c = mk_contractor_ibex_fwdbwd(b, &nc);
     cerr << nc << endl;
     cerr << b << endl;
-    auto input1 = c.input();
-    auto output1 = c.input();
-    cerr << "Input  (BEFORE) : ";  input1.display(cerr) << endl;
-    cerr << "Output (BEFORE) : "; output1.display(cerr) << endl;
+    auto input_before = c.input();
+    auto output_before = c.output();
+    cerr << "Input  (BEFORE) : ";  input_before.display(cerr) << endl;
+    cerr << "Output (BEFORE) : "; output_before.display(cerr) << endl;
     b = c.prune(b, opensmt_ctx->getConfig());
     cerr << b << endl;
-    auto input2 = c.input();
-    auto output2 = c.output();
-    cerr << "Input  (AFTER)  : ";  input2.display(cerr) << endl;
-    cerr << "Output (AFTER)  : "; output2.display(cerr) << endl;
+    auto input_after = c.input();
+    auto output_after = c.output();
+    cerr << "Input  (AFTER)  : ";  input_after.display(cerr) << endl;
+    cerr << "Output (AFTER)  : "; output_after.display(cerr) << endl;
+    for (auto ctc : c.used_constraints()) {
+        cerr << "Used constraint : " << *ctc << endl;
+    }
+    REQUIRE((input_after[0]  && !input_after[1]  && !input_after[2]));
+    REQUIRE((output_after[0] && !output_after[1] && !output_after[2]));
+    auto used_ctcs = c.used_constraints();
+    REQUIRE(used_ctcs.size() == 1);
+    REQUIRE(used_ctcs.find(&nc) != used_ctcs.end());
     opensmt_del_context(ctx);
 }
 
 TEST_CASE("ibex_fwdbwd_02") {
-    opensmt_init();
+    cerr << "===================================================\n";
     opensmt_context ctx = opensmt_mk_context(qf_nra);
     OpenSMTContext * opensmt_ctx = static_cast<OpenSMTContext *>(ctx);
     opensmt_set_precision(ctx, 0.0000001);
@@ -90,21 +99,29 @@ TEST_CASE("ibex_fwdbwd_02") {
     contractor c = mk_contractor_ibex_fwdbwd(b, &nc);
     cerr << nc << endl;
     cerr << b << endl;
-    auto input1 = c.input();
-    auto output1 = c.input();
-    cerr << "Input  (BEFORE) : "; input1.display(cerr)  << endl;
-    cerr << "Output (BEFORE) : "; output1.display(cerr) << endl;
+    auto input_before = c.input();
+    auto output_before = c.output();
+    cerr << "Input  (BEFORE) : "; input_before.display(cerr)  << endl;
+    cerr << "Output (BEFORE) : "; output_before.display(cerr) << endl;
     b = c.prune(b, opensmt_ctx->getConfig());
     cerr << b << endl;
-    auto input2 = c.input();
-    auto output2 = c.output();
-    cerr << "Input  (AFTER)  : "; input2.display(cerr)  << endl;
-    cerr << "Output (AFTER)  : "; output2.display(cerr) << endl;
+    auto input_after = c.input();
+    auto output_after = c.output();
+    cerr << "Input  (AFTER)  : "; input_after.display(cerr)  << endl;
+    cerr << "Output (AFTER)  : "; output_after.display(cerr) << endl;
+    for (auto ctc : c.used_constraints()) {
+        cerr << "Used constraint : " << *ctc << endl;
+    }
+    REQUIRE((input_after[0]  && input_after[1]  && !input_after[2]));
+    REQUIRE((output_after[0] && output_after[1] && !output_after[2]));
+    auto used_ctcs = c.used_constraints();
+    REQUIRE(used_ctcs.size() == 1);
+    REQUIRE(used_ctcs.find(&nc) != used_ctcs.end());
     opensmt_del_context(ctx);
 }
 
 TEST_CASE("ibex_polytope") {
-    opensmt_init();
+    cerr << "===================================================\n";
     opensmt_context ctx = opensmt_mk_context(qf_nra);
     OpenSMTContext * opensmt_ctx = static_cast<OpenSMTContext *>(ctx);
     opensmt_set_precision(ctx, 0.0000001);
@@ -114,29 +131,47 @@ TEST_CASE("ibex_polytope") {
     Enode * var_x = static_cast<Enode *>(x);
     Enode * var_y = static_cast<Enode *>(y);
     Enode * var_z = static_cast<Enode *>(z);
-    opensmt_expr exp1 = opensmt_mk_abs(ctx, x);
-    opensmt_expr exp2 = opensmt_mk_cos(ctx, x);
-    opensmt_expr exp3 = opensmt_mk_div(ctx, exp1, exp2);
-    opensmt_expr exp4 = opensmt_mk_sin(ctx, x);
-    opensmt_expr eq   = opensmt_mk_eq(ctx, exp4, exp3);
-    Enode * node_eq   = static_cast<Enode *>(eq);
-    node_eq->setPolarity(l_True);
 
-    nonlinear_constraint nc(node_eq, true);
+    // y = cos(x);
+    opensmt_expr eq1 = opensmt_mk_eq(ctx, y,
+                                     opensmt_mk_cos(ctx, x));
+    // x = sin(y);
+    opensmt_expr eq2 = opensmt_mk_eq(ctx, x,
+                                     opensmt_mk_sin(ctx, y));
+    Enode * node_eq1 = static_cast<Enode *>(eq1);
+    Enode * node_eq2 = static_cast<Enode *>(eq2);
+    node_eq1->setPolarity(l_True);
+    node_eq2->setPolarity(l_True);
+
+    nonlinear_constraint nc1(node_eq1, true);
+    nonlinear_constraint nc2(node_eq2, true);
     box b({var_x, var_y, var_z});
-    contractor c = mk_contractor_ibex_polytope(0.001, {var_x, var_y, var_z}, {&nc});
-    cerr << nc << endl;
+    contractor c = mk_contractor_ibex_polytope(0.001, {var_x, var_y, var_z}, {&nc1, &nc2});
+    cerr << nc1 << endl;
+    cerr << nc2 << endl;
     cerr << b << endl;
-    auto input1 = c.input();
-    auto output1 = c.input();
-    cerr << "IBEX_polytope Input  (BEFORE) : ";  input1.display(cerr) << endl;
-    cerr << "IBEX_polytope Output (BEFORE) : "; output1.display(cerr) << endl;
+    auto input_before = c.input();
+    auto output_before = c.output();
+    cerr << "IBEX_polytope Input  (BEFORE) : ";  input_before.display(cerr) << endl;
+    cerr << "IBEX_polytope Output (BEFORE) : "; output_before.display(cerr) << endl;
     b = c.prune(b, opensmt_ctx->getConfig());
     cerr << b << endl;
-    auto input2 = c.input();
-    auto output2 = c.output();
-    cerr << "IBEX_polytope Input  (AFTER)  : ";  input2.display(cerr) << endl;
-    cerr << "IBEX_polytope Output (AFTER)  : "; output2.display(cerr) << endl;
+    auto input_after = c.input();
+    auto output_after = c.output();
+    cerr << "IBEX_polytope Input  (AFTER)  : ";  input_after.display(cerr) << endl;
+    cerr << "IBEX_polytope Output (AFTER)  : "; output_after.display(cerr) << endl;
+
+    // TODO(soonhok): !input_after has to be false.
+    REQUIRE((input_after[0]  && input_after[1]  &&  input_after[2]));
+    REQUIRE((output_after[0] && output_after[1] && !output_after[2]));
+
+    auto used_ctcs = c.used_constraints();
+    for (auto ctc : used_ctcs) {
+        cerr << "Used constraint : " << *ctc << endl;
+    }
+    REQUIRE(used_ctcs.size() == 2);
+    REQUIRE(used_ctcs.find(&nc1) != used_ctcs.end());
+    REQUIRE(used_ctcs.find(&nc2) != used_ctcs.end());
     opensmt_del_context(ctx);
 }
 }  // namespace dreal
