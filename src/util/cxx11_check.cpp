21b2eb07d116ccc9c0042d54d3655fa81248c8e7
Soonho Kong <soonhok@cs.cmu.edu>
2015-07-04 11:23:22 -0400

    feat(util): add cxx11_check

---
+++ src/util/cxx11_check.cpp
@@ -0,0 +1,36 @@
+/*********************************************************************
+Author: Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2015, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include "util/cxx11_check.h"
+#include "util/box.h"
+#include "contractor/contractor.h"
+
+namespace dreal {
+
+void check_nothrow_move_constructible() {
+    static_assert(std::is_nothrow_move_constructible<ibex::IntervalVector>::value,
+                  "class ibex::IntervalVector is not nothrow-move-constructible.");
+    static_assert(std::is_nothrow_move_constructible<ibex::Interval>::value,
+                  "class ibex::Interval is not nothrow-move-constructible.");
+    static_assert(std::is_nothrow_move_constructible<box>::value,
+                  "class dreal::box is not nothrow-move-constructible.");
+    static_assert(std::is_nothrow_move_constructible<contractor>::value,
+                  "class dreal::contractor is not nothrow-move-constructible.");
+}
+}  // namespace dreal
---
+++ src/util/type_name.h
@@ -0,0 +1,58 @@
+/*********************************************************************
+Author: Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2014, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+// The following code is from http://stackoverflow.com/a/20170989
+// which is written by Howard Hinnant (http://stackoverflow.com/users/576911/howard-hinnant)
+// The code is cc-by-sa licensed (http://creativecommons.org/licenses/by-sa/3.0/).
+
+#pragma once
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+#ifndef _MSC_VER
+#include <cxxabi.h>
+#endif
+#include <memory>
+#include <string>
+#include <cstdlib>
+
+namespace dreal {
+template <class T> std::string type_name() {
+    typedef typename std::remove_reference<T>::type TR;
+    std::unique_ptr<char, void(*)(void*)> own
+        (
+#ifndef _MSC_VER
+            abi::__cxa_demangle(typeid(TR).name(), nullptr,
+                                nullptr, nullptr),
+#else
+            nullptr,
+#endif
+            std::free);
+    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    if (std::is_const<TR>::value)
+        r += " const";
+    if (std::is_volatile<TR>::value)
+        r += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        r += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        r += "&&";
+    return r;
+}
+}  // namespace dreal
