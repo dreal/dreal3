a7823f8285eacebd8f0e3455136b5fbc426819ca
Soonho Kong <soonhok@cs.cmu.edu>
2015-07-09 15:57:05 -0400

    refactor(constraint/constraint.cpp): remove debug message

--- src/constraint/constraint.cpp
+++ src/constraint/constraint.cpp
@@ -402,7 +402,6 @@ generic_forall_constraint::generic_forall_constraint(Enode * const e, lbool cons
       m_forall_vars(extract_forall_vars(e->getCdr()->getCdr())),
       m_body(e->getCdr()->getCar()),
       m_polarity(p) {
-    cerr << "m_body = " << m_body << endl;
 }
 unordered_set<Enode *> generic_forall_constraint::get_forall_vars() const {
     return m_forall_vars;
