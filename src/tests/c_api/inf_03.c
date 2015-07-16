412401fcd43031fb35e7cc3553c9f410f94471bf
Soonho Kong <soonhok@cs.cmu.edu>
2015-07-16 07:06:13 -0700

    test(c_api): add inf_01, inf_02, inf_03
    related issue: #143

---
+++ src/tests/c_api/inf_03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "opensmt/api/opensmt_c.h"
+
+// Test Memory Leaks
+int main() {
+    opensmt_init();
+    opensmt_context ctx = opensmt_mk_context(qf_nra);
+    opensmt_expr x = opensmt_mk_unbounded_real_var(ctx, "x");
+    opensmt_expr zero = opensmt_mk_num(ctx, 0.0);
+    opensmt_expr leq = opensmt_mk_leq(ctx, zero, x);
+    opensmt_assert(ctx, leq);
+    opensmt_result res = opensmt_check( ctx );
+    if (res == l_true) {
+        double const x_lb = opensmt_get_lb(ctx, x);
+        double const x_ub = opensmt_get_ub(ctx, x);
+        printf("sat: x = [%f, %f]\n", x_lb, x_ub);
+    } else {
+        printf("unsat\n");
+    }
+    opensmt_del_context(ctx);
+    return 0;
+}
