94a39e554f59cd3fa14bbc49592b101d5d459a48
Daniel Bryce <danbryce410@gmail.com>
2015-08-21 11:52:35 -0500

    feat(heuristic): added heuristic conflict clauses

--- src/opensmt/smtsolvers/CoreSMTSolver.C
+++ src/opensmt/smtsolvers/CoreSMTSolver.C
@@ -1850,7 +1850,7 @@ lbool CoreSMTSolver::search(int nof_conflicts, int nof_learnts)
             DREAL_LOG_INFO << "Found Model after # decisions " << decisions << endl;
           }
 
-	  if ( next == lit_Error )
+	  if ( next == lit_Error)
 	    {
 	      //	      return l_False;
 	      Clause* confl = heuristic->getConflict( );
@@ -1858,7 +1858,7 @@ lbool CoreSMTSolver::search(int nof_conflicts, int nof_learnts)
 		DREAL_LOG_DEBUG << "Analyze Conflict";
 		// CONFLICT
 		conflicts++; conflictC++;
-		if (decisionLevel() == 0)
+		if (decisionLevel() == 0 || confl->size() == 0)
 		  return l_False;
 		
 		//      first = false;
