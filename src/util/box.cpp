5b19107bade0b89bc16cf2f176ead517c4fda9a7
Daniel Bryce <danbryce410@gmail.com>
2015-08-25 21:47:53 -0500

    feat(box): split time dimension unevenly, and fixed bugs in hybrid heuristic

--- src/util/box.cpp
+++ src/util/box.cpp
@@ -231,6 +231,17 @@ tuple<int, box, box> box::bisect(double precision) const {
     }
 }
 
+
+  //select bisection ratio for interval
+double box::get_bisection_ratio(int i) const {
+  if (is_time_variable(i) && m_values[i].contains(0.0)) {
+    DREAL_LOG_DEBUG << "Splitting time variable";
+    return 0.0001;
+  } else {
+    return 0.5;
+  }
+}
+  
 tuple<int, box, box> box::bisect_int_at(int i) const {
     assert(0 <= i && i < m_values.size());
     box b1(*this);
@@ -261,6 +272,7 @@ double box::get_bisection_ratio(int i) const {
   }
 }
   
+// Bisect a box into two boxes by bisecting i-th interval.
 tuple<int, box, box> box::bisect_real_at(int i) const {
     assert(0 <= i && i < m_values.size());
     box b1(*this);
