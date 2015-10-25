53d4fab8b0b6b5671c4691ca97b3bb9c1d7dba20
Soonho Kong <soonhok@cs.cmu.edu>
2015-10-25 10:59:18 -0400

    chore(util) add #pragma once in ibex_variable_hash.h

--- src/util/ibex_variable_hash.h
+++ src/util/ibex_variable_hash.h
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with dReal. If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 
+#pragma once
 #include <cstring>
 #include <functional>
 #include "ibex/ibex.h"
