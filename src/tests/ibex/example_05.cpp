ff49663d9476fe694956aec70e36f1edcbe7a1fb
Soonho Kong <soonhok@cs.cmu.edu>
2015-10-26 00:04:45 -0400

    test(ibex): add example_05.cpp

---
+++ src/tests/ibex/example_05.cpp
@@ -0,0 +1,47 @@
+/*********************************************************************
+Author: Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2015, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include <iostream>
+#include "ibex/ibex.h"
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch/catch.hpp"
+
+using ibex::Variable;
+using ibex::Function;
+using ibex::NumConstraint;
+using ibex::CtcFwdBwd;
+using ibex::IntervalVector;
+using ibex::Interval;
+using ibex::ExprSymbol;
+using ibex::ExprCtr;
+using ibex::ExprConstant;
+using ibex::ExprNode;
+using std::cout;
+using std::endl;
+
+TEST_CASE("ctc_fwdbwd_empty") {
+    NumConstraint c("x1", "x2", "x2 < 0");
+    IntervalVector box(2);
+    box[0] = Interval(0, 0);
+    box[1] = Interval(1.5, 1.5);
+    cout << "box before=" << box << endl;
+    CtcFwdBwd(c).contract(box);
+    cout << "box after=" << box << endl;
+    REQUIRE(box.is_empty() == 1);
+}
