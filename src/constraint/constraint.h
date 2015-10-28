ec06122c00737b23b383b30560984eb016a3bda1
Soonho Kong <soonhok@cs.cmu.edu>
2015-10-28 14:51:57 -0400

    chore(constraint/constraint.h): fix style

--- src/constraint/constraint.h
+++ src/constraint/constraint.h
@@ -127,7 +127,7 @@ public:
     inline Enode * get_time_t() const { return m_time_t; }
     inline Enode * get_inv()    const { return m_inv; }
     forallt_constraint(Enode * const e, std::unordered_set<Enode*> const & var_set, unsigned const flow_id, Enode * const time_0, Enode * const time_t, Enode * const inv);
-    forallt_constraint(Enode * const e, std::unordered_set<Enode*> const & var_set) : forallt_constraint(mk_forallt_constraint(e, var_set)) { };
+    forallt_constraint(Enode * const e, std::unordered_set<Enode*> const & var_set) : forallt_constraint(mk_forallt_constraint(e, var_set)) { }
 
     virtual std::ostream & display(std::ostream & out) const;
 };
