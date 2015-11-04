1bf10f49b4735ad883dd46f9e601983563447976
Sicun Gao <scungao@gmail.com>
2015-11-04 00:06:46 -0500

    api: init dreal api

---
+++ src/tests/c_api/dreal_api_test1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "api/dreal.h"
+
+// Related Issue: https://github.com/dreal/dreal3/issues/143
+
+int main(int argc, char * argv[]) {
+/* julia> X = Var(Float64) # unbounded */
+/* Ex{Float64}(Ptr{Void} @0x000000000704be00,Set{ASCIIString}({"x0"})) */
+/* julia> add!(sin(X) > 0.0) */
+/* Set{ASCIIString}({"x0"}) */
+/* julia> is_satisfiable() */
+/* false */
+/* — */
+    dreal_init();
+    dreal_context ctx = dreal_mk_context(qf_nra);
+    dreal_expr x = dreal_mk_unbounded_real_var(ctx, "x");
+    dreal_expr zero = dreal_mk_num(ctx, 0.0);
+    dreal_expr gt = dreal_mk_gt(ctx, dreal_mk_sin(ctx, x), zero);
+    dreal_push(ctx);
+    dreal_assert(ctx, gt);
+    dreal_result res = dreal_check( ctx );
+    printf( "%s\n\n", res == l_false ? "unsat" : "sat" );
+    dreal_del_context(ctx);
+    return 0;
+}
