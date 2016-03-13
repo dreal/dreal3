2ea1fa951f83180b580899ce2da97ba76fefe44c
Soonho Kong <soonhok@cs.cmu.edu>
2016-03-13 11:45:35 -0400

    chore(api/dreal.{C,h}): add copyright form, clean up whitespaces

--- src/api/dreal.C
+++ src/api/dreal.C
@@ -1,7 +1,27 @@
+/*********************************************************************
+Author: Sicun Gao <sicung@cs.cmu.edu>
+        Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2016, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
 #include <string>
 #include <limits>
 #include <utility>
-#include "dreal.h"
+#include "api/dreal.h"
 #include "opensmt/api/OpenSMTContext.h"
 #include "opensmt/egraph/Egraph.h"
 #include "opensmt/cnfizers/Tseitin.h"
