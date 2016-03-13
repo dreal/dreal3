ab82258cb169f3898dcbb14950c4bb660fada1dd
Soonho Kong <soonhok@cs.cmu.edu>
2016-03-13 11:47:02 -0400

    chore(api/dreal.hh): add copyright form, remove whitespaces

--- src/api/dreal.hh
+++ src/api/dreal.hh
@@ -1,3 +1,23 @@
+/*********************************************************************
+Author: Sicun Gao <sicung@cs.cmu.edu>
+        Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2016, Soonho Kong, Sicun Gao, and Edmund Clarke
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
 #pragma once
 
 #include <iostream>
@@ -34,56 +54,56 @@ private:
 
 };
 
-//All passing by value to make chained compositions easy. Delegating RVO to compilers. 
+//All passing by value to make chained compositions easy. Delegating RVO to compilers.
 expr operator==(expr, expr);
 expr operator==( expr, double );
-expr operator==( double, expr );
+expr operator==(double, expr );
 expr operator>(expr, expr);
-expr operator>( expr, double );
-expr operator>( double, expr );
+expr operator>(expr, double );
+expr operator>(double, expr );
 expr operator<(expr, expr);
-expr operator<( expr, double );
-expr operator<( double, expr );
+expr operator<(expr, double );
+expr operator<(double, expr );
 expr operator<=(expr, expr);
-expr operator<=( expr, double );
-expr operator<=( double, expr );
+expr operator<=(expr, double );
+expr operator<=(double, expr );
 expr operator>=(expr, expr);
-expr operator>=( expr, double );
-expr operator>=( double, expr );
-expr operator+( expr, expr );
-expr operator+( expr, double );
-expr operator+( double, expr );
-expr operator-( expr, expr );
-expr operator-( double, expr );
-expr operator-( expr, double );
-expr operator-( expr );
-expr operator*( expr, expr );
-expr operator*( expr, double );
-expr operator*( double, expr );
-expr operator/( expr, expr );
-expr operator/( expr, double );
-expr operator/( double, expr );
-expr abs( expr );
-expr pow( expr, expr );
-expr pow( expr, double );
-expr pow( double, expr );
-expr operator^( expr, expr);
-expr operator^( expr, double);
-expr operator^( double, expr);
-expr sqrt( expr );
-expr exp( expr );
-expr log( expr );
-expr sin( expr );
-expr cos( expr );
-expr tan( expr );
-expr asin( expr );
-expr acos( expr );
-expr atan( expr );
-expr sinh( expr );
-expr cosh( expr );
-expr tanh( expr );
-expr operator&&( expr, expr );
-expr operator||( expr, expr );
+expr operator>=(expr, double );
+expr operator>=(double, expr );
+expr operator+(expr, expr );
+expr operator+(expr, double );
+expr operator+(double, expr );
+expr operator-(expr, expr );
+expr operator-(double, expr );
+expr operator-(expr, double );
+expr operator-(expr );
+expr operator*(expr, expr );
+expr operator*(expr, double );
+expr operator*(double, expr );
+expr operator/(expr, expr );
+expr operator/(expr, double );
+expr operator/(double, expr );
+expr abs(expr );
+expr pow(expr, expr );
+expr pow(expr, double );
+expr pow(double, expr );
+expr operator^(expr, expr);
+expr operator^(expr, double);
+expr operator^(double, expr);
+expr sqrt(expr );
+expr exp(expr );
+expr log(expr );
+expr sin(expr );
+expr cos(expr );
+expr tan(expr );
+expr asin(expr );
+expr acos(expr );
+expr atan(expr );
+expr sinh(expr );
+expr cosh(expr );
+expr tanh(expr );
+expr operator&&(expr, expr );
+expr operator||(expr, expr );
 expr operator!(expr);
 expr implies(expr, expr);
 expr ite(expr, expr, expr);
@@ -102,48 +122,41 @@ public:
     expr    num(const double);
     expr    num(const int);
     expr    num(const char *);
-    expr    get_value ( expr& );
+    expr    get_value (expr&);
 
-    void    set_verbosity   ( int );
-    void    set_precision   ( const double );
     void    print   ( expr& );
+    void    set_verbosity   (int);
+    void    set_precision   (const double);
     void    reset   ();
     void    push    ();
-    void    pop	    ();
-    void    add	    ( expr& );
-    void    set_domain_lb ( expr&, double );
-    void    set_domain_ub ( expr&, double );
- 
+    void    pop     ();
+    void    add     (expr&);
+    void    set_domain_lb (expr&, double);
+    void    set_domain_ub (expr&, double);
+
     double  get_precision   ();
-    double  get_domain_lb ( expr& );
-    double  get_domain_ub ( expr& );
-    double  get_lb  ( expr& );
-    double  get_ub  ( expr& );
- 
+    double  get_domain_lb (expr&);
+    double  get_domain_ub (expr&);
+    double  get_lb  (expr&);
+    double  get_ub  (expr&);
+
     bool    check   ();
 
-    result  check_assump    ( expr& );
-    result  check_lim_assump( expr& , unsigned );
-    result  get_bool_value  ( expr& );
+    result  check_assump    (expr&);
+    result  check_lim_assump(expr& , unsigned);
+    result  get_bool_value  (expr&);
 
     unsigned	get_conflicts   ();
     unsigned	get_decisions   ();
-  
-    inline  env	    get_ctx() { return cctx; }; 
+
+    env     get_ctx() { return cctx; };
 
     //todo
-    void    print_proof               ( const char * );
-    void    print_interpolant         ( const char * );
+    void    print_proof               (const char *);
+    void    print_interpolant         (const char *);
 
 private:
-
     env cctx;
     std::vector<expr>    expr_table;
-
 };
-
-
 }
-
-
-
