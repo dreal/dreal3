a8ad228e5b6c499fec8c3e19f273844aa2e6e273
Soonho Kong <soonhok@cs.cmu.edu>
2016-03-15 00:53:14 -0400

    feat(cmake/Modules/cpplint.py): support .hh and .C

--- src/cmake/Modules/cpplint.py
+++ src/cmake/Modules/cpplint.py
@@ -499,7 +499,7 @@ _line_length = 80
 
 # The allowed extensions for file names
 # This is set by --extensions flag.
-_valid_extensions = set(['cc', 'h', 'cpp', 'cu', 'cuh'])
+_valid_extensions = set(['C', 'cc', 'h', 'hh', 'cpp', 'cu', 'cuh'])
 
 def ParseNolintSuppressions(filename, raw_line, linenum, error):
   """Updates the global list of error-suppressions.
