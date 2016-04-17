b4509f7fb43a94c9974d7ad4e35f35ee986423da
Daniel Bryce <danbryce410@gmail.com>
2016-04-17 15:51:24 -0500

    version used for ecai16 submission

--- benchmarks/network/dribble/run-exp.sh
+++ benchmarks/network/dribble/run-exp.sh
@@ -2,7 +2,8 @@
 
 ulimit -t 1200
 
-MAX=10
+MIN=21
+MAX=30
 
 TIMEFORMAT='%2R'
 OPTS="--no-hybrid-clause-learning"
@@ -10,42 +11,59 @@ OPTS="--no-hybrid-clause-learning"
 
 
 
-echo "++++++++++++++++++ Single NonLinear Heuristic +++++++++++++++"
 
 
- 
-for ((i=2; i <= $MAX; i++)); do {
-    K=`expr $i \* 4`
-    time dReach -u $K -l $K -r -n dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
-}    ; done
 
-echo "++++++++++++++++++ Single NonLinear  +++++++++++++++"
 
- 
-for ((i=2; i <= $MAX; i++)); do {
-    K=`expr $i \* 4`
-     time dReach -u $K -l $K -d -n dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
-}    ; done
 
 
 echo "++++++++++++++++++ Flat NonLinear Heuristic +++++++++++++++"
 
 
  
-for ((i=2; i <= $MAX; i++)); do {
+for ((i=$MIN; i <= $MAX; i++)); do {
+    K=`expr $i \* 2`
+    time dReach -u $K -l $K -r dribble-flat.drh --precision 0.1 --stat  2>&1 > /tmp/dan3
+}    ; done
+
+echo "++++++++++++++++++ Flat NonLinear Heuristic no-cl +++++++++++++++"
+
+
+ 
+for ((i=$MIN; i <= $MAX; i++)); do {
     K=`expr $i \* 2`
     time dReach -u $K -l $K -r dribble-flat.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3
 }    ; done
 
 
 
-echo "++++++++++++++++++ Single NonLinear Composed +++++++++++++++"
+echo "++++++++++++++++++ Single NonLinear Heuristic +++++++++++++++"
 
 
  
-for ((i=2; i <= $MAX; i++)); do {
+for ((i=$MIN; i <= $MAX; i++)); do {
     K=`expr $i \* 4`
-     time dReach -u $K -l $K -d -c dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
+    time dReach -u $K -l $K -r -n dribble-net.drh --precision 0.1 --stat  2>&1 > /tmp/dan3 
+}    ; done
+
+
+
+echo "++++++++++++++++++ Single NonLinear Heuristic no-cl +++++++++++++++"
+
+
+ 
+for ((i=$MIN; i <= $MAX; i++)); do {
+    K=`expr $i \* 4`
+    time dReach -u $K -l $K -r -n dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
+}    ; done
+
+
+echo "++++++++++++++++++ Single NonLinear  +++++++++++++++"
+
+ 
+for ((i=$MIN; i <= $MAX; i++)); do {
+    K=`expr $i \* 4`
+     time dReach -u $K -l $K -d -n dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
 }    ; done
 
 
@@ -55,15 +73,37 @@ for ((i=2; i <= $MAX; i++)); do {
 
 echo "++++++++++++++++++ Flat NonLinear  +++++++++++++++"
 
-for ((i=2; i <= $MAX; i++)); do {
+for ((i=$MIN; i <= $MAX; i++)); do {
     K=`expr $i \* 2`
      time dReach -u $K -l $K -d dribble-flat.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
 }    ; done
+
+
 echo "++++++++++++++++++ Single NonLinear Heuristic Composed +++++++++++++++"
 
 
  
-for ((i=2; i <= $MAX; i++)); do {
+for ((i=$MIN; i <= $MAX; i++)); do {
+    K=`expr $i \* 4`
+     time dReach -u $K -l $K -r -c dribble-net.drh --precision 0.1 --stat  2>&1 > /tmp/dan3 
+}    ; done
+
+
+echo "++++++++++++++++++ Single NonLinear Heuristic Composed no-cl +++++++++++++++"
+
+
+ 
+for ((i=$MIN; i <= $MAX; i++)); do {
     K=`expr $i \* 4`
      time dReach -u $K -l $K -r -c dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
+}    ; done
+
+
+echo "++++++++++++++++++ Single NonLinear Composed +++++++++++++++"
+
+
+ 
+for ((i=$MIN; i <= $MAX; i++)); do {
+    K=`expr $i \* 4`
+     time dReach -u $K -l $K -d -c dribble-net.drh --precision 0.1 --stat $OPTS 2>&1 > /tmp/dan3 
 }    ; done
--- benchmarks/network/gen/run-exp.sh
+++ benchmarks/network/gen/run-exp.sh
@@ -10,6 +10,146 @@ OPT=--no-hybrid-clause-learning
 
 
 
+
+
+
+
+echo "++++++++++++++++++ Flat nonLinear  +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+     A=`expr 2 \* $i`
+    ./gen-flat-nonlinear.sh $i > gen-$i-flat-nonlinear.drh
+    time  dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  gen-$i-flat-nonlinear.drh --precision 0.1 --stat     2>&1 > /dev/null
+} ; done
+
+
+echo "++++++++++++++++++ Flat nonLinear Heuristic +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+     A=`expr 2 \* $i`
+    ./gen-flat-nonlinear.sh $i > gen-$i-flat-nonlinear.drh
+    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-nonlinear.drh --precision 0.1 --stat     2>&1 > /dev/null
+} ; done
+
+echo "++++++++++++++++++ Flat nonLinear Heuristic no-cl +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+     A=`expr 2 \* $i`
+    ./gen-flat-nonlinear.sh $i > gen-$i-flat-nonlinear.drh
+    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-nonlinear.drh --precision 0.1 --stat  $OPT   2>&1 > /dev/null
+} ; done
+
+
+     echo "++++++++++++++++++ Composed Nonlinear  +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -c gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan
+}; done
+
+
+
+
+echo "++++++++++++++++++ Composed Nonlinear Heuristic +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -c gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan
+}; done
+
+
+echo "++++++++++++++++++ Composed Nonlinear Heuristic no-cl +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -c gen-$i-single-nonlinear.drh --precision 0.1 --stat $OPT 2>&1 > /tmp/dan
+}; done
+     echo "++++++++++++++++++ Single Nonlinear  +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -n gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan 
+    } ; done
+
+
+
+echo "++++++++++++++++++ Single Nonlinear Heuristic +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+    time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -n gen-$i-single-nonlinear.drh --precision 0.1 --stat   2>&1 > /tmp/dan
+} ; done
+echo "++++++++++++++++++ Single Nonlinear Heuristic no-cl +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+    time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -n gen-$i-single-nonlinear.drh --precision 0.1 --stat  $OPT 2>&1 > /tmp/dan
+} ; done
+
+
+
+
+
+
+
+
+
+echo "++++++++++++++++++ Composed Nonlinear  +++++++++++++++"
+
+
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
+    A=`expr 4 \* $i`
+     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -c gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan
+} ; done
+
+echo "++++++++++++++++++ Single Linear  +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-linear-single.sh $i > gen-$i-single-linear.drh
+    A=`expr 4 \* $i`
+     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -n gen-$i-single-linear.drh --precision 0.1 --stat 2>&1 > /tmp/dan 
+    } ; done
+
+
+
+echo "++++++++++++++++++ Single Linear Heuristic +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
+    A=`expr 4 \* $i`
+    time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-linear.drh --precision 0.1 --stat $OPT 2>&1 > /tmp/dan 
+} ; done
+
+
+echo "++++++++++++++++++ Flat Linear Heuristic +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+     A=`expr 2 \* $i`
+    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
+    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-linear.drh --precision 0.1 --stat     2>&1 > /dev/null
+} ; done
+
+echo "++++++++++++++++++ Flat Linear Heuristic, no learn +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+     A=`expr 2 \* $i`
+    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
+    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-linear.drh --precision 0.1 --stat  $OPT   2>&1 > /dev/null
+} ; done
+
+echo "++++++++++++++++++ Flat Linear  +++++++++++++++"
+for ((i=0; i <= $MAX; i++)); do {
+     A=`expr 2 \* $i`
+    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
+    time  dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  gen-$i-flat-linear.drh --precision 0.1 --stat     2>&1 > /dev/null
+} ; done
+
 echo "++++++++++++++++++  Linear Heuristic, No learning +++++++++++++++"
 
 
@@ -39,26 +179,7 @@ for ((i=$MIN; i <= $MAX; i++)); do {
     A=`expr 4 \* $i`
      time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -c gen-$i-single-linear.drh --precision 0.1 --stat $OPT  2>&1 > /tmp/dan
 } ; done
-echo "++++++++++++++++++ Flat Linear Heuristic +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-     A=`expr 2 \* $i`
-    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
-    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-linear.drh --precision 0.1 --stat     2>&1 > /dev/null
-} ; done
 
-echo "++++++++++++++++++ Flat Linear Heuristic, no learn +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-     A=`expr 2 \* $i`
-    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
-    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-linear.drh --precision 0.1 --stat  $OPT   2>&1 > /dev/null
-} ; done
-
-echo "++++++++++++++++++ Flat Linear  +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-     A=`expr 2 \* $i`
-    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
-    time  dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  gen-$i-flat-linear.drh --precision 0.1 --stat     2>&1 > /dev/null
-} ; done
 
 echo "++++++++++++++++++ Composed Linear  +++++++++++++++"
 
@@ -94,87 +215,3 @@ for ((i=0; i <= $MAX; i++)); do {
 
 
 
-echo "++++++++++++++++++ Single Nonlinear Heuristic +++++++++++++++"
-
-
-for ((i=0; i <= $MAX; i++)); do {
-    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
-    A=`expr 4 \* $i`
-    time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -n gen-$i-single-nonlinear.drh --precision 0.1 --stat  $OPT 2>&1 > /tmp/dan
-} ; done
-
-
-echo "++++++++++++++++++ Flat nonLinear Heuristic +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-     A=`expr 2 \* $i`
-    ./gen-flat-nonlinear.sh $i > gen-$i-flat-nonlinear.drh
-    time   dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-nonlinear.drh --precision 0.1 --stat     2>&1 > /dev/null
-} ; done
-
-
-
-
-
-
-
-
-
-
-
-echo "++++++++++++++++++ Flat nonLinear  +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-     A=`expr 2 \* $i`
-    ./gen-flat-nonlinear.sh $i > gen-$i-flat-nonlinear.drh
-    time  dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  gen-$i-flat-nonlinear.drh --precision 0.1 --stat     2>&1 > /dev/null
-} ; done
-
-
-
-
-
-
-echo "++++++++++++++++++ Single Linear  +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-    ./gen-linear-single.sh $i > gen-$i-single-linear.drh
-    A=`expr 4 \* $i`
-     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -n gen-$i-single-linear.drh --precision 0.1 --stat 2>&1 > /tmp/dan 
-    } ; done
-
-
-
-
-
-echo "++++++++++++++++++ Single Nonlinear  +++++++++++++++"
-
-
-for ((i=0; i <= $MAX; i++)); do {
-    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
-    A=`expr 4 \* $i`
-     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -n gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan 
-    } ; done
-
-
-
-echo "++++++++++++++++++ Composed Nonlinear Heuristic +++++++++++++++"
-
-
-for ((i=0; i <= $MAX; i++)); do {
-    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
-    A=`expr 4 \* $i`
-     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  -c gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan
-
-echo "++++++++++++++++++ Composed Nonlinear  +++++++++++++++"
-
-
-for ((i=0; i <= $MAX; i++)); do {
-    ./gen-nonlinear-single.sh $i > gen-$i-single-nonlinear.drh
-    A=`expr 4 \* $i`
-     time dReach -u `expr 3 + $A` -l `expr 3 + $A` -d  -c gen-$i-single-nonlinear.drh --precision 0.1 --stat 2>&1 > /tmp/dan
-    } ; done
-
-echo "++++++++++++++++++ Single Linear Heuristic +++++++++++++++"
-for ((i=0; i <= $MAX; i++)); do {
-    ./gen-flat-linear.sh $i > gen-$i-flat-linear.drh
-    A=`expr 4 \* $i`
-    time dReach -u `expr 3 + $A` -l `expr 3 + $A` -r  gen-$i-flat-linear.drh --precision 0.1 --stat $OPT 2>&1 > /tmp/dan 
-} ; done
