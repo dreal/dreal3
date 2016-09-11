fe550063af36065ff8ae32cd162010edcd13cf79
Soonho Kong <soonho.kong@tri.global>
2016-09-10 22:03:58 -0400

    test(cpp_api): add dump.cpp
    related issue #299

---
+++ src/tests/cpp_api/dump.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************
+Author: Sicun Gao <sicung@cs.cmu.edu>
+        Soonho Kong <soonhok@cs.cmu.edu>
+
+dReal -- Copyright (C) 2013 - 2016, the dReal Team
+
+dReal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+dReal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with dReal. If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************/
+
+#include <iostream>
+#include <sstream>
+#include "api/dreal.h"
+
+using namespace dreal;
+int main() {
+    solver s;
+    expr x = s.var("x", -1, 1);
+    s.add(x + sin(x) < 0);
+    s.dump_dr_file("wrong");  // it writes to wrongdumped.dr
+    return 0;
+}
