a41b3d67fc4a9b857a2b588e8dde31f9f6ce37be
Soonho Kong <soonho.kong@tri.global>
2016-10-04 11:43:12 -0400

    feat(cmake/Modules/FindCLP.cmake): use pkg_check_modules
    related issue #307

--- src/cmake/Modules/FindCLP.cmake
+++ src/cmake/Modules/FindCLP.cmake
@@ -4,42 +4,22 @@
 #  CLP_INCLUDE_DIRS - The CLP include directories
 #  CLP_LIBRARIES - The libraries needed to use CLP
 
-
+find_package(PkgConfig)
+pkg_check_modules(CLP clp)
+set(CLP_DEFINITIONS ${CLP_CFLAGS_OTHER})
 find_path(CLP_INCLUDE_DIR
-          NAMES "ClpConfig.h"
-          PATHS "/usr/include/coin"
-                "/usr/local/include/clp/coin"
-          NO_DEFAULT_PATH)
-
+    NAMES "ClpConfig.h"
+    PATHS "/usr/include/coin"
+    "/usr/local/include/clp/coin"
+    NO_DEFAULT_PATH)
 get_filename_component(CLP_INCLUDE_DIR ${CLP_INCLUDE_DIR} DIRECTORY)
-
-find_path(COINUTILS_INCLUDE_DIR
-          NAMES "CoinPragma.hpp"
-          PATHS "/usr/include/coin"
-                "/usr/local/include/coinutils/coin"
-          NO_DEFAULT_PATH)
-
-find_library(CLP_LIBRARY
-             NAMES Clp
-             PATHS "/usr/lib"
-                   "/usr/local/lib"
-                   "/usr/lib/coin")
-find_library(COINUTILS_LIBRARY
-             NAMES CoinUtils
-             PATHS "/usr/lib"
-                   "/usr/local/lib")
-
-set(CLP_INCLUDE_DIRS "${CLP_INCLUDE_DIR};${COINUTILS_INCLUDE_DIR}")
-set(CLP_LIBRARIES "${CLP_LIBRARY};${COINUTILS_LIBRARY}")
+set(CLP_INCLUDE_DIRS "${CLP_INCLUDE_DIRS};${CLP_INCLUDE_DIR}")
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set CLP_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(CLP
-    DEFAULT_MSG
-    CLP_LIBRARY
-    COINUTILS_LIBRARY
-    CLP_INCLUDE_DIR
-    COINUTILS_INCLUDE_DIR)
+find_package_handle_standard_args(CLP DEFAULT_MSG CLP_INCLUDE_DIR CLP_LIBRARIES)
 
-mark_as_advanced(CLP_INCLUDE_DIR CLP_LIBRARIES)
+mark_as_advanced(CLP_INCLUDE_DIR CLP_LIBRARIES )
+
+if(CLP_FOUND)
+    message(STATUS "Clp found (includes: ${CLP_INCLUDE_DIRS}, libs: ${CLP_LIBRARIES})")
+endif(CLP_FOUND)
