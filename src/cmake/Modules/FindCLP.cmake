3db50b144e4c1b5647d6c7b6b7c9f912e16656d8
Soonho Kong <soonho.kong@tri.global>
2016-10-05 14:42:13 -0400

    fix(cmake/Modules/FindCLP.cmake): check bzip2 when find clp

--- src/cmake/Modules/FindCLP.cmake
+++ src/cmake/Modules/FindCLP.cmake
@@ -5,20 +5,25 @@
 #  CLP_LIBRARIES - The libraries needed to use CLP
 
 find_package(PkgConfig)
-pkg_check_modules(CLP clp)
-set(CLP_DEFINITIONS ${CLP_CFLAGS_OTHER})
-find_path(CLP_INCLUDE_DIR
-    NAMES "ClpConfig.h"
-    PATHS "/usr/include/coin"
-    "/usr/local/include/clp/coin"
-    NO_DEFAULT_PATH)
-get_filename_component(CLP_INCLUDE_DIR ${CLP_INCLUDE_DIR} DIRECTORY)
-set(CLP_INCLUDE_DIRS "${CLP_INCLUDE_DIRS};${CLP_INCLUDE_DIR}")
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CLP DEFAULT_MSG CLP_INCLUDE_DIR CLP_LIBRARIES)
-
-mark_as_advanced(CLP_INCLUDE_DIR CLP_LIBRARIES )
+# The result of `pkg-config clp --libs` includes "-lbz2" but Ubuntu
+# package 'coinor-lib-dev' doesn't include libbz2-dev as a
+# dependency. As a result, if you only do `sudo apt-get install
+# coinor-clp-dev`, you will not have `libbz2-dev` installed and dReal
+# won't compile since it can't find `-lbz2`.
+find_package(BZip2 QUIET)
+if (BZip2_FOUND)
+    set(CLP_DEFINITIONS ${CLP_CFLAGS_OTHER})
+    find_path(CLP_INCLUDE_DIR
+        NAMES "ClpConfig.h"
+        PATHS "/usr/include/coin"
+        "/usr/local/include/clp/coin"
+        NO_DEFAULT_PATH)
+    get_filename_component(CLP_INCLUDE_DIR ${CLP_INCLUDE_DIR} DIRECTORY)
+    set(CLP_INCLUDE_DIRS "${CLP_INCLUDE_DIRS};${CLP_INCLUDE_DIR}")
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(CLP DEFAULT_MSG CLP_INCLUDE_DIR CLP_LIBRARIES)
+    mark_as_advanced(CLP_INCLUDE_DIR CLP_LIBRARIES )
+endif()
 
 if(CLP_FOUND)
     message(STATUS "Clp found (includes: ${CLP_INCLUDE_DIRS}, libs: ${CLP_LIBRARIES})")
